ef4dcce53834a83932b7deeab0edfe10abf182b3
Gleiss.Martin@gmail.com <Gleiss.Martin@gmail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2012-11-24 22:50:59 +0000

    - new: shutter - reorg

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
  
     // get config-variables 
     require_once 'config.php';
+    require_once const_path_system.'functions.php';
+    require_once const_path_system.'functions_twig.php';
     
     // init parameters
     $request = array_merge($_GET, $_POST);
@@ -25,7 +27,7 @@
         or is_file(const_path."pages/base/".$request['page'].".html") )
     {
         // init template engine
-        require_once const_path.'lib/Twig/Autoloader.php';
+        require_once const_path.'vendor/twig/Autoloader.php';
         Twig_Autoloader::register();
         
         $loader = new Twig_Loader_Filesystem(const_path.'pages/'.config_pages);
@@ -80,14 +82,15 @@
                 $twig->addGlobal($key, $val);
         }
         
-        $twig->addFilter('_', new Twig_Filter_Function('filter_concat'));
-        $twig->addFilter('round', new Twig_Filter_Function('filter_round'));
-        $twig->addFilter('bit', new Twig_Filter_Function('filter_bit'));
-        $twig->addFilter('substr', new Twig_Filter_Function('filter_substr'));
+        $twig->addFilter('_',           new Twig_Filter_Function('twig_concat'));
+        $twig->addFilter('round',       new Twig_Filter_Function('twig_round'));
+        $twig->addFilter('bit',         new Twig_Filter_Function('twig_bit'));
+        $twig->addFilter('substr',      new Twig_Filter_Function('twig_substr'));
+        $twig->addFilter('smartdate',   new Twig_Filter_Function('twig_smartdate'));
         
-        $twig->addFunction('dir', new Twig_Function_Function('fn_dir'));
-        $twig->addFunction('once', new Twig_Function_Function('fn_once'));
-        $twig->addFunction('docu', new Twig_Function_Function('fn_docu'));    
+        $twig->addFunction('dir',       new Twig_Function_Function('twig_dir'));
+        $twig->addFunction('once',      new Twig_Function_Function('twig_once'));
+        $twig->addFunction('docu',      new Twig_Function_Function('twig_docu'));    
         
         // load template
         try
@@ -111,150 +114,5 @@
     }
     else
         header("HTTP/1.0 404 Not Found");
-    
-   
-// -----------------------------------------------------------------------------
-// Filter extensioins
-// -----------------------------------------------------------------------------
-
-    function filter_concat($val, $text = '')
-    {
-        if ($text != '')
-            $val .= "_".$text;
-            
-        return $val;
-    } 
-    
-    function filter_round($val, $precision = 0)
-    {
-        return round($val, $precision);
-    }
-
-    function filter_bit($val)
-    {
-        return ($val ? 'true' : 'false');
-    }
-    
-    function filter_substr($val, $start, $end = null)
-    {
-        if ($end)    
-            $ret = substr($val, $start, $end);
-        else
-            $ret = substr($val, $start);
-               
-        return $ret;
-    } 
-
- 
-// -----------------------------------------------------------------------------
-// Function extensioins
-// -----------------------------------------------------------------------------
-   
-    function fn_once($name)
-    {
-        $ret = false;
-        
-        static $singular = array();
-        
-        if (!isset($singular[$name]))
-            $ret = true;    
-        
-        $singular[$name] += 1;       
-        
-        return $ret;
-    }
-    
- 	function fn_dir($dir, $filter = '(.*)')
-    {
-        $ret = array();
-        
-        clearstatcache();
-        
-        // read directory contents
-        $dirlist = dir(const_path.$dir);
-        	
-        while (($item = $dirlist->read()) !== false)
-        {
-            if ($item != '.' and $item != '..' and$item != '.svn' and substr($item, 0, 1) != '_')
-            {
-                if (preg_match("#".$filter."$#i", $item, $itemparts) > 0)
-                    $ret[] = array("file" => $itemparts[0], "name" => $itemparts[1], "label" => ucfirst($itemparts[1]));
-		    }
-   		}
-   		
-		$dirlist->close();
-	   
-		return $ret;
- 		}       
-    
-  
- 	function fn_docu($filename)
-    {
-        $ret = array();
-        $rettmp = array();
-        
-        $file = file_get_contents($filename);
-        
-        // Header
-        preg_match_all('#.+?@(.+?)\W+(.*)#i', substr($file, 0, strpos($file, '*/') + 2), $header);
-        
-        // Body 
-        preg_match_all('#\/\*\*[\r\n](.+?)\*\/.+?\{\% macro(.+?)\%\}#is', strstr($file, '*/'), $widgets);
-        
-        if (count($widgets[2]) > 0)
-        {
-            foreach ($widgets[2] as $no => $macro)
-            {
-                preg_match_all('#(.+?)\((.+?)\)#i', $macro, $desc);
-                $rettmp[$no]['name'] = trim($desc[1][0]);
-                $rettmp[$no]['params'] = trim($desc[2][0]);
-            }
-            
-            foreach ($widgets[1] as $no => $docu)
-            {
-                $rettmp[$no]['desc'] = trim(str_replace('* ', '', substr($docu, 0, strpos($docu, '@'))));
-            
-                // Header-Tags 
-                foreach($header[1] as $headerno => $headertag)
-                {
-                    if ( !($headertag == "author" and trim($header[2][$headerno]) == "Martin Gleiß") )
-                        $ret[$headertag] = trim($header[2][$headerno]);
-                }
-                
-                $rettmp[$no]['subpackage'] = substr(mb_strtolower(basename($filename), 'UTF-8'), 0, -5);
-                $rettmp[$no]['command'] = $rettmp[$no]['subpackage'].".".$rettmp[$no]['name'];
-                $rettmp[$no]['call'] = $rettmp[$no]['command']."(".$rettmp[$no]['params'].")";
-            
-                // Widget-Tags
-                preg_match_all('#.+?@(.+?)\W+(.*)#i', $docu, $tags);
-                
-                $param = 0;
-                $params = explode(',', $rettmp[$no]['params']);
-                
-                foreach($tags[1] as $id => $tag)
-                {
-                    if ($tag == 'param')
-                    {
-                        $rettmp[$no]['param'][trim($params[$param++])] = trim($tags[2][$id]);     
-                    }
-                    else
-                        $rettmp[$no][$tag] = trim($tags[2][$id]);     
-                }    
-            }
-    	               
-    	    foreach ($rettmp as $attributes)
-    	       $ret[$attributes['subpackage'].'.'.$attributes['name']] = $attributes;
-        }
-        else
-        {
-            foreach($header[1] as $headerno => $headertag)
-            {
-                if ( !($headertag == "author" and trim($header[2][$headerno]) == "Martin Gleiß") )
-                    $ret[$headertag] = trim($header[2][$headerno]);
-            }
-        }
-         
-        return $ret;
- 	}      
 
 ?>
