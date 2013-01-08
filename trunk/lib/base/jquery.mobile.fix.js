81864bb24033a55d801079eb86dd20c33dcc6cf9
Gleiss.Martin@gmail.com <Gleiss.Martin@gmail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2013-01-08 14:17:56 +0000

    - finished: basic.slider

---
+++ trunk/lib/base/jquery.mobile.fix.js
@@ -0,0 +1,46 @@
+/**
+ * -----------------------------------------------------------------------------
+ * @package     smartVISU
+ * @author      Martin Gleiß
+ * @copyright   2012
+ * @license     GPL <http://www.gnu.de>
+ * ----------------------------------------------------------------------------- 
+ */
+ 
+
+// jquery.mobile fix: slider trogger 'start' and 'stop'
+$(document).on({
+    "mousedown touchstart": function () { $(this).siblings("input").trigger("start"); },
+    "mouseup touchend": function () { $(this).siblings("input").trigger("stop"); }
+}, ".ui-slider, .ui-slider-vertical, .ui-slider-bottomup, .ui-slider-semicircle-box");
+
+
+// jquery.mobile: normalize events
+// basic.flip
+$(document).on("change", 'select[data-role="slider"]', function() {
+    $(this).trigger('click');
+}); 
+
+// basic.slider
+$(document).on("stop", 'input[data-type="range"]', function() { 
+    // unlock and trigger manually to avoid twice
+    if ($(this).attr('lock') == 'true')
+        $(this).trigger('click');
+    
+    $(this).attr('lock', 'false');
+});
+ 
+$(document).on("change", 'input[data-type="range"]', function() { 
+    // use a lock to fire event only every 400ms
+    if ($(this).attr('lock') == 'false')
+    {
+        $(this).attr('lock', 'true');
+        setTimeout(
+            "if($('#" + this.id + "').val() != " + $(this).val() + " && $('#" + this.id + "').attr('lock') == 'true')" +
+            "   $('#" + this.id + "').trigger('click');" +
+            "$('#" + this.id + "').attr('lock', 'false');", 400);
+    }
+    // no click on init    
+    if($(this).attr('lock') === undefined)
+        $(this).attr('lock', 'false');  
+});
