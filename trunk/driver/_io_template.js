fcfec19908648cab9cf4fd36cd8ee420e26464b7
Gleiss.Martin@gmail.com <Gleiss.Martin@gmail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2013-01-19 16:05:58 +0000

    - new icons, some improvments

--- trunk/driver/_io_template.js
+++ trunk/driver/_io_template.js
@@ -115,7 +115,10 @@ var io = {
     * 
     * @param      the item         
     */         
-    update: function(item) {
+    update: function(item, val) {
+        if (val)
+            io.buffer[item] = val;
+            
         var listeners = io.listeners[item];
         if (listeners) {
             for (var i = 0; i < listeners.length; i++) {
@@ -134,7 +137,7 @@ var io = {
                 }
             }
         }
-	}, 
+	},
 	
   /**
     * Does a read-request and adds the result to the buffer
@@ -152,7 +155,6 @@ var io = {
     * @param      the value 
     */
     write: function(item, val) {
-        io.buffer[item] = val;
         // TODO
     },
     
