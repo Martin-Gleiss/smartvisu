868cd64b5d8d48ad75923750c3270c98f046e6bd
Gleiss.Martin@gmail.com <Gleiss.Martin@gmail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2013-01-29 10:38:02 +0000

    Release v2.2

---
+++ tags/v2.2/driver/io_domotiga.js
@@ -0,0 +1,294 @@
+/**
+ * -----------------------------------------------------------------------------
+ * @package     smartVISU
+ * @author      Martin Gleiß
+ * @copyright   2012
+ * @license     GPL <http://www.gnu.de>
+ * ----------------------------------------------------------------------------- 
+ */
+ 
+ 
+/**
+ * Class for controlling all communication with a connected system. There are 
+ * simple I/O functions, and complex functions for real-time values. 
+ */  
+var io = {
+
+    // the adress
+    adress:     '',
+    
+    // the port
+    port:       '',
+    
+    // a list with all values, all communication goes through the buffer
+    buffer: new Object(),
+
+    // a list with all listeners
+    listeners: new Object(),
+
+
+// -----------------------------------------------------------------------------
+// H E L P E R   F U N C T I O N S
+// -----------------------------------------------------------------------------
+// These functions are private and only called from other functions within.
+   
+  /**
+    *  Checks if there are any listeners 
+    *       
+    *  @param      a specific obj is tested     
+    *  @return     true, if there are any listeners     
+    */            
+    listening: function(item, update) {
+        var ret = false;
+        
+        if (item !== undefined && update !== undefined) {
+            for(var i = 0; i < io.listeners[item].length; i++) {
+                if ((io.listeners[item][i].update).toString() == (update).toString())
+                    ret = true;    
+            }
+        }
+        else
+            for (var i in io.listeners)
+                ret = true;
+        
+        return ret;        
+    },
+
+    
+// -----------------------------------------------------------------------------
+// P U B L I C   F U N C T I O N S
+// -----------------------------------------------------------------------------
+// The function-interfaces in this paragraph should not be changed. They are 
+// called from the widgets.
+
+  /**
+    * Add a item to listen on. 
+    *     
+    * @param      the item 
+    * @param      the function for update the widget 
+    */         	
+	add: function(item, update) { 	
+    	// 'item' is a array if listening on more than one
+    	if (item instanceof Array) {
+    		for(var i = 0; i < item.length; i++) {
+                if (item[i] != '') {
+                    if (!io.listeners[item[i]])
+                        io.listeners[item[i]] = Array();
+                    
+                    if (!io.listening(item[i], update))    
+                        io.listeners[item[i]].push({item: item, update: update});
+                }
+            }
+            
+        // 'item' is a string
+        } else if (item != '') {
+            if (!io.listeners[item])
+                io.listeners[item] = Array();
+            
+            if (!io.listening(item, update))
+                io.listeners[item].push({item: item, update: update});
+        }
+    },
+    
+  /**
+    * Remove a item from the listeners
+    *      
+    * @param      the item, (optional, removes all if no item is given)
+    */         
+    remove: function(item) {
+        if (item !== undefined)
+            delete io.listeners[item];
+        else
+            io.listeners = new Object();  
+    },
+    
+  /**
+    * List all items and the number of listeners in console.log
+    */         
+	list: function() {
+	    var items = 0;
+        var listeners = 0;
+        for (var item in this.listeners) {
+            console.log("[io] " + io.listeners[item].length + " widget(s) listen on '" + item + "'");
+            listeners += io.listeners[item].length;
+            items++;
+        }
+        console.log("[io] --> " + listeners + " widget(s) listen on " + items + " items.");
+    },
+    
+  /**
+    * Update all listeners hearing on a item
+    * 
+    * @param      the item         
+    */         
+    update: function(item, val) {
+        if (val !== undefined)
+            io.buffer[item] = val;
+
+        var listeners = io.listeners[item];
+        if (listeners) {
+            for (var i = 0; i < listeners.length; i++) {
+                {
+                // more than on item? than all values in repsonse
+                if (listeners[i].item instanceof Array)
+                {
+                    vals = Array();
+                    for(var j = 0; j < listeners[i].item.length; j++) {
+                        vals.push(io.buffer[listeners[i].item[j]]);
+                    }
+                    listeners[i].update(vals);
+                }
+                else
+                    listeners[i].update(io.buffer[item]);
+                }
+            }
+        }
+	}, 
+	
+  /**
+    * Does a read-request and adds the result to the buffer
+    * 
+    * @param      the item      
+    */
+	read: function(item) {
+    },
+    
+  /**
+    * Does a write-request with a value
+    *
+    * @param      the item 
+    * @param      the value 
+    */
+    write: function(item, val) {
+        io.send({'cmd': 'item', 'id': item, 'val': val});
+    },
+    
+  /**
+    * Initializion of the driver
+    *
+    * @param      the ip or url to the system (optional)
+    * @param      the port on which the connection should be made (optional) 
+    */
+   	init: function(address, port) {
+   	    io.address = address;
+   	    io.port = port;
+        io.remove();
+        io.open();
+    },
+    
+  /**
+    * Lets the driver work
+    */
+    run: function(realtime) {
+        if (io.socket.readyState > 1)
+            io.open();
+        else
+            io.monitor();          
+    },
+		
+
+// -----------------------------------------------------------------------------
+// C O M M U N I C A T I O N   F U N C T I O N S
+// -----------------------------------------------------------------------------
+// The functions in this paragraph may be changed. They are all private and are
+// only be called from the public functions above. You may add or delete some
+// to fit your requirements and your connected system.
+	 
+  /**
+    * This driver version
+    */
+    version: '0.1',
+
+  /**
+    * This driver uses a websocket
+    */
+    socket: false,
+    
+  /**
+    * Opens the connection and add some handlers
+    */
+    open: function() {
+        io.socket = new WebSocket('ws://' + io.address + ':' + io.port + '/');
+        io.socket.onopen = function(){
+            io.send({'cmd': 'proto', 'ver': io.version});
+            io.monitor();
+        };
+        io.socket.onmessage = function(event) {
+            var item, val;
+            var data = JSON.parse(event.data);
+            console.log("[io.domotiga] receiving data: " + event.data);
+            switch(data.cmd) {
+                case 'item': // item
+                    for (var i = 0; i < data.items.length; i=i+2) {
+                        item = data.items[i];
+                        val = data.items[i+1];
+                        console.log("[io.domotiga]: update item: " + item + " val: " + val);
+                        io.update(item, val);
+                    };
+/*
+                    for (var i = 0; i < data.items.length; i++) {
+                        item = data.items[i][0];
+                        val = data.items[i][1];
+                        if ( data.items[i].length > 2 ) {
+                            // not supported: data.p[i][2] options for visu
+                        };
+                        console.log("[io.domotiga] update item: " + item + " val: " + val);
+                        io.update(item, val);
+                    };
+*/
+                    break;
+                case 'rrd':
+                    break;
+                case 'dialog':
+                    notify.info(data.header, data.content);
+                case 'proto':
+                    var proto = data.ver;
+                    if (proto != io.version) {
+                        notify.info('Driver: domotiga', 'Protocol mismatch<br \>Driver is at version v' + io.version + '<br />DomotiGa is at version v' + proto);
+                    };
+                case 'log':
+                    break;
+            };
+        };
+        io.socket.onerror = function(error){
+            notify.error('Driver: DomotiGa', 'Websocket error: ' + error);
+        };
+        io.socket.onclose = function(){
+            notify.error('Driver: DomotiGa', 'Could not connect to DomotiGa server!');
+        };
+    },
+    
+  /**
+    * Sends data to the connected system
+    */         
+    send: function(data) {
+        if (io.socket.readyState == 1) {
+            io.socket.send(unescape(encodeURIComponent(JSON.stringify(data))));
+            console.log('[io.domotiga] sending data: ' + JSON.stringify(data));
+        // } else {
+        // notify.error('Driver: domotiga', 'Websocket not available. Could not send data!');
+            }
+    },
+
+  /**
+    * Monitors the items
+    */
+    monitor: function() {
+        var monitor = new Array();
+        
+        for (var item in io.listeners)
+            monitor.push(item);
+
+        io.send({'cmd': 'monitor', 'items': monitor});
+    },
+
+
+  /**
+    * Closes the connection
+    */
+    close: function() {
+        io.socket.close(); 
+        io.socket = null;
+    }
+
+}
---
+++ tags/v2.2/lib/functions_twig.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * -----------------------------------------------------------------------------
+ * @package     smartVISU
+ * @author      Martin Gleiß
+ * @copyright   2012
+ * @license     GPL <http://www.gnu.de>
+ * ----------------------------------------------------------------------------- 
+ */
+ 
+
+// -----------------------------------------------------------------------------
+// Filters for Twig
+// -----------------------------------------------------------------------------
+ 
+ 
+    function twig_concat($val, $text = '')
+    {
+        if ($text != '')
+            $val .= "_".$text;
+            
+        return $val;
+    } 
+    
+    function twig_round($val, $precision = 0)
+    {
+        return round($val, $precision);
+    }
+
+    function twig_bit($val)
+    {
+        return ($val ? 'true' : 'false');
+    }
+    
+    function twig_substr($val, $start, $end = null)
+    {
+        if ($end)    
+            $ret = substr($val, $start, $end);
+        else
+            $ret = substr($val, $start);
+               
+        return $ret;
+    } 
+
+    function twig_smartdate($val, $format = 'date')
+    {
+        return smartdate($format, $val);
+    }
+    
+ 
+// -----------------------------------------------------------------------------
+// General functions for Twig
+// -----------------------------------------------------------------------------
+      
+    function twig_uid($str1, $str2 = '', $str3 = '')
+    {
+        $ret = str_replace('.', '_', $str1.($str2 != '' ? '-'.$str2 : '').($str3 != '' ? '-'.$str3 : ''));
+        
+        return $ret;
+    }
+
+    function twig_once($name)
+    {
+        $ret = false;
+        
+        static $singular = array();
+        
+        if (!isset($singular[$name]))
+            $ret = true;    
+        
+        $singular[$name] = true;       
+        
+        return $ret;
+    }
+
+    function twig_isfile($file)
+    {
+        return is_file(const_path.$file);
+    }
+    
+ 	function twig_dir($dir, $filter = '(.*)')
+    {
+        $ret = array();
+        
+        clearstatcache();
+        
+        // read directory contents
+        $dirlist = dir(const_path.$dir);
+        	
+        while (($item = $dirlist->read()) !== false)
+        {
+            if ($item != '.' and $item != '..' and$item != '.svn' and substr($item, 0, 1) != '_')
+            {
+                if (preg_match("#".$filter."$#i", $item, $itemparts) > 0)
+                {
+                    $name = str_replace("_", " ", $itemparts[1]);
+                    $ret[$name] = array("file" => $itemparts[0], "name" => $itemparts[1], "label" => ucfirst($name));
+                }
+		    }
+   		}
+   		
+		$dirlist->close();
+
+        // sort
+        ksort($ret);
+
+		return $ret;
+ 		}       
+    
+  
+ 	function twig_docu($filename)
+    {
+        $ret = array();
+        $rettmp = array();
+        
+        $file = file_get_contents($filename);
+        
+        // Header
+        preg_match_all('#.+?@(.+?)\W+(.*)#i', substr($file, 0, strpos($file, '*/') + 2), $header);
+        
+        // Body 
+        preg_match_all('#\/\*\*[\r\n](.+?)\*\/.+?\{\% macro(.+?)\%\}#is', strstr($file, '*/'), $widgets);
+        
+        if (count($widgets[2]) > 0)
+        {
+            foreach ($widgets[2] as $no => $macro)
+            {
+                preg_match_all('#(.+?)\((.+?)\)#i', $macro, $desc);
+                $rettmp[$no]['name'] = trim($desc[1][0]);
+                $rettmp[$no]['params'] = trim($desc[2][0]);
+            }
+            
+            foreach ($widgets[1] as $no => $docu)
+            {
+                $rettmp[$no]['desc'] = trim(str_replace('* ', '', substr($docu, 0, strpos($docu, '@'))));
+            
+                // Header-Tags 
+                foreach($header[1] as $headerno => $headertag)
+                {
+                    if ( !($headertag == "author" and trim($header[2][$headerno]) == "Martin Gleiß") )
+                        $ret[$headertag] = trim($header[2][$headerno]);
+                }
+                
+                $rettmp[$no]['subpackage'] = substr(mb_strtolower(basename($filename), 'UTF-8'), 0, -5);
+                $rettmp[$no]['command'] = $rettmp[$no]['subpackage'].".".$rettmp[$no]['name'];
+                $rettmp[$no]['call'] = $rettmp[$no]['command']."(".$rettmp[$no]['params'].")";
+            
+                // Widget-Tags
+                preg_match_all('#.+?@(.+?)\W+(.*)#i', $docu, $tags);
+                
+                $param = 0;
+                $params = explode(',', $rettmp[$no]['params']);
+                
+                foreach($tags[1] as $id => $tag)
+                {
+                    if ($tag == 'param')
+                    {
+                        $rettmp[$no]['param'][trim($params[$param++])] = trim($tags[2][$id]);     
+                    }
+                    else
+                        $rettmp[$no][$tag] = trim($tags[2][$id]);     
+                }    
+            }
+    	               
+    	    foreach ($rettmp as $attributes)
+    	       $ret[$attributes['subpackage'].'.'.$attributes['name']] = $attributes;
+        }
+        else
+        {
+            foreach($header[1] as $headerno => $headertag)
+            {
+                if ( !($headertag == "author" and trim($header[2][$headerno]) == "Martin Gleiß") )
+                    $ret[$headertag] = trim($header[2][$headerno]);
+            }
+        }
+         
+        return $ret;
+ 	}      
+
+
+// -----------------------------------------------------------------------------
+// Special functions for Twig
+// -----------------------------------------------------------------------------
+     
+    function twig_array2script($gads)
+    {
+        $ret = '';
+        
+        if (is_array($gads))
+        {
+            foreach ($gads as $gad)
+                $ret .= "'".$gad."', ";
+                
+            $ret = 'Array('.substr($ret, 0, -2).')';    
+        }
+        else
+            $ret = "'".$gads."'";
+        
+        return $ret;
+    }
+?>
