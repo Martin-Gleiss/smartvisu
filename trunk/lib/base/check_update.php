63a4efcca3776a3cacc7af59a1f2722232df028f
Gleiss.Martin@gmail.com <Gleiss.Martin@gmail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2013-04-11 21:20:52 +0000

    RC v2.4 Updatemechanism

---
+++ trunk/lib/base/check_update.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * -----------------------------------------------------------------------------
+ * @package     smartVISU
+ * @author      Martin Gleiß
+ * @copyright   2012
+ * @license     GPL <http://www.gnu.de>
+ * ----------------------------------------------------------------------------- 
+ */
+ 
+ 
+    // get config-variables 
+    require_once '../../config.php';
+     
+    // init parameters
+    $request = array_merge($_GET, $_POST);
+    
+    $url ="http://code.google.com/feeds/p/smartvisu/downloads/basic";
+    
+    // get contents
+    $data = file_get_contents($url);
+    $xml = simplexml_load_string($data);
+    
+    $file = basename((string)$xml->entry->id);
+    
+    $ret["local"] = (strlen($request["local"]) > 3 ? $request["local"] : str_replace('.', '.0', $request["local"]));
+    
+    $remote = substr(strstr($file, '_'), 1, -4);
+    $ret["remote"] = (strlen($remote) > 3 ? $remote : str_replace('.', '.0', $remote));
+    
+	if ((float)$ret["remote"] > (float)$ret["local"])
+	{
+	    $ret["update"] = true;
+		$ret["icon"] = 'icons/or/message_attention.png';
+		$ret["text"] = 'please make a update!';
+	}
+	else
+	{
+	    $ret["update"] = false;
+		$ret["icon"] = 'icons/gn/message_ok.png';
+		$ret["text"] = 'smartVISU is actual (no update needed)';
+	}
+   
+    echo json_encode($ret);
+?>
+
+
