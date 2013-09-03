712ee3ab29d54546b6c09389bb9e377061ff63bd
raik.alber@googlemail.com <raik.alber@googlemail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2013-09-03 15:55:41 +0000

    update for convertTemplates.php
    now, you can convert your templates between eibd, linknx and smarthome.py (tested: eibd <-> smarthome.py, linknx -> eibd)
    read help for more information! it is important to understand the options!

--- vendor/tools/convertTemplates.php
+++ vendor/tools/convertTemplates.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
  * User: raikalber
- * Date: 25.01.13
+ * Date: 03.09.2013
  * Time: 16:54
- * To change this template use File | Settings | File Templates.
+ * Version: 2.0
  */
 
-$shortOptions = 's:d:i::o::h';
-$longOptions = array('source:', 'dest:', 'input::', 'output::', 'help');
+error_reporting(0);
+$shortOptions = 's:d:i::o::c::h';
+$longOptions = array('source:', 'dest:', 'input::', 'output::', 'cfilespath::', 'help');
 
 $possibleSchema = array('linknx', 'eibd', 'smarthome.py', 'domotiga');
 
@@ -18,11 +18,13 @@ $goOn = true;
 function printHelp() {
     global $possibleSchema;
 
-    echo 'Usage: ' .$_SERVER['SCRIPT_FILENAME'] . ' ' . '-s|--source=sourceSchema' . ' ' . '-d|--dest=destinationSchema' . ' ' . '[--input=inputDirectory]' . ' ' . '[--output=outputDirectory]' . "\n\n";
+    echo 'Usage: ' .$_SERVER['SCRIPT_FILENAME'] . ' ' . '-s|--source=sourceSchema' . ' ' . '-d|--dest=destinationSchema' . ' ' . '[--input=inputDirectory]' . ' ' . '[--output=outputDirectory]' . ' ' . '[--cfilespath=converterFilesPath]' . "\n\n";
     echo '      sourceSchema one of: ' . implode(', ', $possibleSchema) ."\n";
     echo '      destinationSchema one of: ' . implode(', ', $possibleSchema) ."\n";
+    echo '      sourceSchema and destinationSchema should not be the same' . "\n";
     echo '      optional inputDirectory, default "./" '."\n";
-    echo '      optional outputDirectory, default "./", filenames are changed to "destinationSchema.filename.extension" '."\n";
+    echo '      optional outputDirectory, default "./", if not set, filenames are changed to "destinationSchema.filename.extension" '."\n";
+    echo '      optional converterFilesPath, default "./", path to eg. linknx.xml or smarthome.py item files" '."\n";
     echo "\n";
 }
 
@@ -41,8 +43,29 @@ if (array_key_exists('d', $sendOptions) || array_key_exists('dest', $sendOptions
     $goOn = false;
 }
 
-$inputDirectory = array_key_exists('input', $sendOptions) ? $sendOptions['input'] : './';
-$outputDirectory = array_key_exists('output', $sendOptions) ? $sendOptions['output'] : './';
+if (array_key_exists('input', $sendOptions)) {
+    $inputDirectory =  $sendOptions['input'];
+} else {
+    $inputDirectory = './';
+}
+
+if (array_key_exists('output', $sendOptions)) {
+    $outputDirectory =  $sendOptions['output'];
+    if (substr($outputDirectory,-1,1) != '/') {
+        $outputDirectory .= '/';
+    }
+} else {
+    $outputDirectory = './'.$destinationSchema .'.';
+}
+
+if (array_key_exists('cfilespath', $sendOptions)) {
+    $converterFilesPath =  $sendOptions['cfilespath'];
+    if (substr($converterFilesPath, -1,1 != '/')) {
+        $converterFilesPath .= '/';
+    }
+} else {
+    $converterFilesPath = './';
+}
 
 if (!in_array($sourceSchema,$possibleSchema) || !in_array($destinationSchema, $possibleSchema) || $sourceSchema == $destinationSchema) {
     $goOn = false;
@@ -59,8 +82,8 @@ if ($sourceSchema == 'linknx') {
 
     $returnData = array('ids' => array(), 'gads' => array());
 
-    if (file_exists('linknx.xml')) {
-        $xml = simplexml_load_file('linknx.xml');
+    if (file_exists($converterFilesPath.'linknx.xml')) {
+        $xml = simplexml_load_file($converterFilesPath.'linknx.xml');
 
         foreach ($xml->objects->object as $oneObject) {
 
@@ -69,8 +92,13 @@ if ($sourceSchema == 'linknx') {
 
         }
     } else {
-        exit('Konnte linknx.xml nicht öffnen.');
+        exit('Konnte '.$converterFilesPath.'linknx.xml nicht öffnen.');
     }
+}
+
+if ($sourceSchema == 'smarthome.py' || $destinationSchema == 'smarthome.py') {
+
+    $returnData = readSmarthomePyItems($converterFilesPath);
 }
 
 $dp = dir($inputDirectory);
@@ -78,16 +106,165 @@ if (!file_exists($outputDirectory)) {
     mkdir($outputDirectory);
 }
 
-while (false !== ($entry = $dp->read())) {
-    if ( strpos($entry, '.html') ) {
-        $inputData = file_get_contents($inputDirectory . $entry);
+$inputDataArray = array();
 
-        foreach($returnData['ids'] as $oneIdKey => $oneIdArray) {
+while (false !== ($dirEntry = $dp->read())) {
+    if ( strpos($dirEntry, '.html') ) {
 
-            $inputData = str_replace($oneIdKey, $oneIdArray['gad'].'/'.$oneIdArray['dpt'], $inputData);
-        }
-
-        file_put_contents($outputDirectory . $entry, $inputData);
+        $inputDataArray[$dirEntry] = file_get_contents($inputDirectory . $dirEntry);
     }
 }
 $dp->close();
+
+foreach ($inputDataArray as $inputDataFilename => $inputDataText) {
+
+    switch ($destinationSchema) {
+
+        case 'eibd':
+
+            foreach($returnData['ids'] as $oneIdKey => $oneIdArray) {
+
+                $inputDataText = str_replace($oneIdKey, $oneIdArray['gad'].'/'.$oneIdArray['dpt'], $inputDataText);
+            }
+            $inputDataArray[$inputDataFilename] = $inputDataText;
+            break;
+        case 'smarthome.py':
+
+            foreach ($returnData['gads'] as $oneIdKey => $oneIdArray) {
+
+                $searchString = "#'". $oneIdKey . "/.*?'#";
+                $inputDataText = preg_replace($searchString, "'".trim($oneIdArray['id'])."'", $inputDataText);
+            }
+            $inputDataArray[$inputDataFilename] = $inputDataText;
+            break;
+    }
+}
+
+foreach ($inputDataArray as $inputDataFilename => $inputDataText) {
+
+    file_put_contents($outputDirectory . $inputDataFilename, $inputDataText);
+}
+
+
+function readSmarthomePyItems($itemSourcePath) {
+
+    $returnData = array('ids' => array(), 'gads' => array());
+
+    $dp = dir($itemSourcePath);
+
+    while (false !== ($entry = $dp->read())) {
+
+        $layers = array();
+
+        if ( substr($entry, 0, 6) == 'items_')  {
+
+            $fp = fopen($itemSourcePath. $entry, 'r');
+
+            while ($oneLine = trim(fgets($fp))) {
+
+                $actualLayer = substr_count($oneLine, '[');
+
+                if ($actualLayer > 0) {
+
+                    $oneLine = strtr($oneLine, array('[' => '', ']' => ''));
+                    $layers[$actualLayer] = $oneLine;
+                    $itemNameFound = false;
+                    $dataSaved = false;
+                    $itemName = '';
+                    $dpt = false;
+
+                } else {
+
+                    $knxCommand = explode ('=', $oneLine);
+
+                    if (trim($knxCommand[0]) == 'knx_dpt') {
+                        switch (trim($knxCommand[1])) {
+                            case "1":
+                                $dpt = '1.001';
+                                break;
+                            case "2":
+                                $dpt = '2.xxx';
+                                break;
+                            case "3":
+                                $dpt = '3.xxx';
+                                break;
+                            case "4002":
+                                $dpt = '4.002';
+                                break;
+                            case "5":
+                                $dpt = '5.xxx';
+                                break;
+                            case "5001":
+                                $dpt = '5.001';
+                                break;
+                            case "6":
+                                $dpt = '6.xxx';
+                                break;
+                            case "7":
+                                $dpt = '7.xxx';
+                                break;
+                            case "8":
+                                $dpt = '8.xxx';
+                                break;
+                            case "9":
+                                $dpt = '9.xxx';
+                                break;
+                            case "10":
+                                $dpt = '10.xxx';
+                                break;
+                            case "11":
+                                $dpt = '11.xxx';
+                                break;
+                            case "12":
+                                $dpt = '12.xxx';
+                                break;
+                            case "13":
+                                $dpt = '13.xxx';
+                                break;
+                            case "14":
+                                $dpt = '14.xxx';
+                                break;
+                            case "16000":
+                                $dpt = '16.000';
+                                break;
+                            case "16001":
+                                $dpt = '16.001';
+                                break;
+                            case "17":
+                                $dpt = '17.xxx';
+                                break;
+                            case "20":
+                                $dpt = '20.xxx';
+                                break;
+                            case "24":
+                                $dpt = '24.xxx';
+                                break;
+                        }
+                    }
+
+                    if ( ((trim($knxCommand[0]) == 'knx_listen') || (trim($knxCommand[0]) == 'knx_send')) && $itemNameFound === false) {
+
+                        $itemNameFound = true;
+
+                        foreach ($layers as $oneItem) {
+
+                            $itemName .= $oneItem .'.';
+                        }
+                        $itemName = substr($itemName, 0,-1);
+
+                    }
+
+                    if ( ($itemName) && ($dpt) && ($dataSaved === false)) {
+                        $dataSaved = true;
+                        $returnData['gads'][trim($knxCommand[1])] = array('id' => trim($itemName), 'dpt' => $dpt);
+                        $returnData['ids'][trim($itemName)]       = array('gad' => trim($knxCommand[1]), 'dpt' => $dpt);
+
+                    }
+                }
+            }
+
+        }
+    }
+    return $returnData;
+}
+
