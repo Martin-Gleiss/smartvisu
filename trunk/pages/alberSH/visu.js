c091430c54fc23211e06cfdc32bba5901e74ef87
raik.alber@googlemail.com <raik.alber@googlemail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2013-10-09 13:28:44 +0000

    house alber with smarthome.py backend structure rebuild,  unused files removed

---
+++ trunk/pages/alberSH/visu.js
@@ -0,0 +1,20 @@
+$(document).delegate('[data-widget="calc.calculate"]', {
+    'update': function (event, response) {
+        calculated = 0;
+        for (var i = 0; i < response.length; i++) {
+            if ($(this).attr('data-mode') == 'sub') {
+                calculated = calculated - response[i];
+            } else {
+                calculated = calculated + response[i];
+            }
+        }
+        if ($(this).attr('data-mode') == 'avg') {
+            calculated = calculated / i ;
+        }
+        calculated =   eval($(this).attr('data-calcstart') + calculated + $(this).attr('data-calcend'));
+        if ($(this).attr('data-round') != '') {
+            calculated = calculated.toFixed($(this).attr('data-round'));
+        }
+        $("#" + this.id ).html(calculated + $(this).attr('data-unit'));
+    }
+});
