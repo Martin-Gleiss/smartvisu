84447748b22924785789a4dd1ac1338bc555f709
axel.otterstaetter@googlemail.com <axel.otterstaetter@googlemail.com@8b1cb026-df5a-56d3-d591-4adab6cb69f6>
2015-04-07 09:06:15 +0000

    New dynamic icon light added

--- trunk/widgets/widget.js
+++ trunk/widgets/widget.js
@@ -1356,3 +1356,22 @@ $(document).delegate('svg[data-widget="icon.windsock"]', {
 		$('#' + this.id + ' #part3').attr('points', pt.toString());
 	}
 });
+
+// ----- icon.light ---------------------------------------------------------
+$(document).delegate('svg[data-widget="icon.light"]', {
+	'update': function (event, response) {
+		// response is: {{ gad_value }}, {{ gad_switch }}
+        var val = Math.round(response[0] / $(this).attr('data-max') * 10);
+        // Iterate over all child elements
+        var i=1;
+        $('#' + this.id + ' g#light-rays line').each(function(){
+            if (val >= i) {
+                $(this).css("visibility", "visible");
+            } 
+            else {
+                $(this).css("visibility", "hidden");
+            }
+            i++;
+        });
+	}
+});
