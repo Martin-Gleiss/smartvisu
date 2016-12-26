77fb723cdc51dae41e59fea64de1a90e793ab93e
Bonze255 <fragger255@hotmail.com>
2016-12-26 12:07:20 +0100

    Create config.php
    add snow-funktion entry

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+  * -----------------------------------------------------------------------------
+  * @package     smartVISU
+  * @author      Martin Gleiß
+  * @copyright   2012
+  * @license     GPL [http://www.gnu.de]
+  * -----------------------------------------------------------------------------
+  */
+
+
+	define('config_pages', 'holly.smarthome.py');
+	define('config_design', 'night');
+	define('config_cache', false);
+	define('config_animation', false);
+	define('config_title', 'YOUR NAME [smartVISU]');
+	define('config_lang', 'en');
+	define('config_driver', 'smarthome.py');
+	define('config_driver_address', 'smarthome.local');
+	define('config_driver_port', '2424');
+	define('config_driver_realtime', true);
+	define('config_weather_service', 'offline');
+	define('config_weather_location', 'Germany/Bayern/Würzburg');
+	define('config_weather_key', '');
+	define('config_phone_service', 'offline');
+	define('config_phone_server', '192.168.x.x');
+	define('config_phone_user', '');
+	define('config_phone_pass', '');
+	define('config_calendar_service', 'offline');
+	define('config_calendar_url', 'http://www.google.com/calendar/feeds/...');
+	define('config_js', 'min.js');
+	define('snow', 'true');
+?>
