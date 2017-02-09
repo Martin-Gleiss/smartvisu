8c5a5ab309812f0abe6bb6aa281969efc021524a
smaiLee <stefan@stwidmer.ch>
2017-02-09 13:48:31 +0100

    fix wrongly displayd error message on saving confuguration

--- pages/base/configure.php
+++ pages/base/configure.php
@@ -14,47 +14,44 @@ header('Content-Type: application/json');
 
 // just clear pagecache
 if($_GET['clear_cache']) {
-
-	delTree(const_path.'temp/pagecache');
-	$ret = array('title' => 'Configuration', 'text' => 'Pagecache cleared.');
-	echo json_encode($ret);
-
+	$success = delTree(const_path.'temp/pagecache');
+	if($success)
+		echo json_encode(array('title' => 'Configuration', 'text' => 'Pagecache cleared.'));
+	else { // save fails
+		header("HTTP/1.0 600 smartVISU Config Error");
+		echo json_encode(array('title' => 'Configuration', 'text' => 'Error deleting page cache!<br />Please check the file permissions temp/pagecache'));
+	}
 }
+// save configuration
+else if(isset($_GET['target'])) {
+	$config = new config();
+	$success = $config->save($_GET['target'], $_POST, $_GET['pages']);
+	if($success) {
+		if($_POST['cache'] != 'true')
+			delTree(const_path.'temp/pagecache');
+
+		echo json_encode(array('title' => 'Configuration', 'text' => 'Configuration changes saved.'));
+	}
+	else { // save fails
+		header("HTTP/1.0 600 smartVISU Config Error");
+		echo json_encode(array('title' => 'Configuration', 'text' => 'Error saving configuration!<br />Please check the file permissions on "config.php" (it must be writeable)!'));
+	}
+}
+// read configuration
 else {
+	$config = new config();
 
-	// save configuration
-	if(isset($_GET['target'])) {
-		$config = new config();
+	$sources = array('all');
+	if(isset($_GET['source']))
+		$sources = $_GET['source'];
+	if(!is_array($sources)) $sources = array($sources);
 
-		if($config->save($_GET['target'], $_POST, $_GET['pages']) === true) {
-			if($_POST['cache'] != 'true')
-				delTree(const_path.'temp/pagecache');
-
-			echo json_encode(array('title' => 'Configuration', 'text' => 'Configuration changes saved.'));
-		}
-		else { // save fails
-			header("HTTP/1.0 600 smartVISU Config Error");
-			return array('title' => 'Configuration',
-				'text' => 'Error saving configuration!<br />Please check the file permissions on "config.php" (it must be writeable)!');
-		}
+	$result = array();
+	foreach($sources as $source) {
+		if($source != null && $source != '')
+			$result[$source] = $config->get($source);
 	}
 
-	// read configuration
-	else {
-		$config = new config();
-
-		$sources = array('all');
-		if(isset($_GET['source']))
-			$sources = $_GET['source'];
-		if(!is_array($sources)) $sources = array($sources);
-
-		$result = array();
-		foreach($sources as $source) {
-			if($source != null && $source != '')
-				$result[$source] = $config->get($source);
-		}
-
-		echo(json_encode($result));
-	}
+	echo(json_encode($result));
 }
 ?>
