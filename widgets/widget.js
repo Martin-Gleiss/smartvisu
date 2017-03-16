1d7c3893aca5637ddb2ef16abb9ddc42c0b2123f
smaiLee <stefan@stwidmer.ch>
2017-03-16 12:56:59 +0100

    service URLs as item (prefixed by 'url:')
    thereby calendar data is buffered in widget.buffer for page changes.
    Additonally this is a first step for getting service data from backend (see #143)

--- widgets/widget.js
+++ widgets/widget.js
@@ -952,108 +952,114 @@ $(document).on('pagecreate', function (bevent, bdata) {
 
 	// ----- calendar.list --------------------------------------------------------
 	$(bevent.target).find('div[data-widget="calendar.list"]').on( {
-		'repeat': function (event) {
+		'update': function(event, response) {
+			var data = response[0];
 			var node = $(this);
-			$.getJSON('lib/calendar/service/'+node.attr('data-service')+'.php?count='+node.attr('data-count')+'&calendar='+node.attr('data-calendar'), function (data) {
 
-				var calendars = node.attr('data-calendar').explode();
-				var calcolors = {};
-				$.each(node.attr('data-color').explode(), function(i, color) {
-					calcolors[(calendars[i]||'').toLowerCase()] = color;
+			var calendars = node.attr('data-calendar').explode();
+			var calcolors = {};
+			$.each(node.attr('data-color').explode(), function(i, color) {
+				calcolors[(calendars[i]||'').toLowerCase()] = color;
+			});
+
+			var ul = node.children('ul:first').empty();
+
+			$.each(data, function(index, entry) {
+				// "pseudo clone" entry to not mute original object in widget.buffer
+				entry = Object.create(entry);
+
+				// parse start and end as Date
+				if(isNaN(entry.start)) // legacy: start in format 'y-m-d H:i:s'
+					entry.start = new Date(Date.parse('20' + entry.start));
+				else // start as timestamp
+					entry.start = new Date(entry.start*1000);
+
+				if(isNaN(entry.end)) // legacy: end in format 'y-m-d H:i:s'
+					entry.end = new Date(Date.parse('20' + entry.end));
+				else // end as timestamp
+					entry.end = new Date(entry.end*1000);
+
+				// build period string to display
+				var period;
+				// Start and end on same day: show day only once
+				if(entry.end.transUnit('date') == entry.start.transUnit('date'))
+					period = entry.start.transUnit('date') + ' ' + entry.start.transUnit('time') + ' - ' + entry.end.transUnit('time');
+				// Full day entrys: don't show time
+				else if (entry.start.getHours()+entry.start.getMinutes()+entry.start.getSeconds() == 0
+					&& entry.end.getHours()+entry.end.getMinutes()+entry.end.getSeconds() == 0) {
+					entry.end.setDate(entry.end.getDate()-1); // subtract one day from end
+					if(entry.end.transUnit('date') == entry.start.transUnit('date')) // One day only: Show just start date
+						period = entry.start.transUnit('date');
+					else // Multiple days: Show start and end date
+						period = entry.start.transUnit('date') + ' - ' + entry.end.transUnit('date');
+				}
+				else
+					period = entry.start.transUnit('date') + ' ' + entry.start.transUnit('time') + ' - ' + entry.end.transUnit('date') + ' ' + entry.end.transUnit('time');
+
+				// handle calendar_event_format in lang.ini
+				$.each(sv_lang.calendar_event_format, function(pattern, attributes) {
+					if(entry.title.toLowerCase().indexOf(pattern.toLowerCase()) > -1) { // event title contains pattern
+						// set each defined property
+						$.each(attributes, function(prop, val) {
+							entry[prop] = val;
+						});
+					}
 				});
 
-				var ul = node.children('ul:first').empty();
-
-				$.each(data, function(index, entry) {
-					
-					// parse start and end as Date
-					if(isNaN(entry.start)) // legacy: start in format 'y-m-d H:i:s'
-						entry.start = new Date(Date.parse('20' + entry.start));
-					else // start as timestamp
-						entry.start = new Date(entry.start*1000);
-
-					if(isNaN(entry.end)) // legacy: end in format 'y-m-d H:i:s'
-						entry.end = new Date(Date.parse('20' + entry.end));
-					else // end as timestamp
-						entry.end = new Date(entry.end*1000);
-
-					// build period string to display
-					var period;
-					// Start and end on same day: show day only once
-					if(entry.end.transUnit('date') == entry.start.transUnit('date'))
-						period = entry.start.transUnit('date') + ' ' + entry.start.transUnit('time') + ' - ' + entry.end.transUnit('time');
-					// Full day entrys: don't show time
-					else if (entry.start.getHours()+entry.start.getMinutes()+entry.start.getSeconds() == 0
-						&& entry.end.getHours()+entry.end.getMinutes()+entry.end.getSeconds() == 0) {
-						entry.end.setDate(entry.end.getDate()-1); // subtract one day from end
-						if(entry.end.transUnit('date') == entry.start.transUnit('date')) // One day only: Show just start date
-							period = entry.start.transUnit('date');
-						else // Multiple days: Show start and end date
-							period = entry.start.transUnit('date') + ' - ' + entry.end.transUnit('date');
-					}
-					else
-						period = entry.start.transUnit('date') + ' ' + entry.start.transUnit('time') + ' - ' + entry.end.transUnit('date') + ' ' + entry.end.transUnit('time');
-
-					// handle calendar_event_format in lang.ini
-					$.each(sv_lang.calendar_event_format, function(pattern, attributes) {
-						if(entry.title.toLowerCase().indexOf(pattern.toLowerCase()) > -1) { // event title contains pattern
-							// set each defined property
-							$.each(attributes, function(prop, val) {
-								entry[prop] = val;
-							});
-						}
-					});
-
-					// handle tags in event description
-					var tags = (entry.content||'').replace(/\\n/,'\n').match(/@(.+?)\W+(.*)/igm) || [];
-					$.each(tags, function(i, tag) {
-						// parse tag
-						tag = tag.match(/@(.+?)\W+(.*)/i);
-						// prepend # if color is hexadecimal number of 3 or 6 digits
-						if(tag[1] == 'color' && /^([0-9a-f]{3}){1,2}\W*$/i.test(tag[2]))
-							tag[2] = '#'+tag[2];
-						// apply tag to events properties
-						entry[tag[1]] = tag[2];
-					});
-
-					// amend icon path/filename
-					if(entry.icon) {
-						// add default path if icon has no path
-						if(entry.icon.indexOf('/') == -1)
-							entry.icon = 'icons/ws/'+entry.icon;
-						// add svg suffix if icon has no suffix
-						if(entry.icon.indexOf('.') == -1)
-							entry.icon = entry.icon+'.svg';
-					}
-					
-					// add entry
-					var a = $('<a>').append(
-						$('<img class="icon">').css('background', entry.color || 'transparent').attr('src', entry.icon || 'pages/base/pics/trans.png')
-					).append(
-						$('<div class="color">').css('background', calcolors[(entry.calendarname||'').toLowerCase()] || entry.calendarcolor || node.attr('data-color').explode()[0] || '#666666')
-					).append(
-						$('<h3>').text(entry.title)
-					).append(
-						$('<p>').text(period)
-					).appendTo(
-					 $('<li data-icon="false">').appendTo(ul)
-					);
-					if(entry.link)
-						a.attr('href', entry.link);
-					if(entry.where)
-						a.append($('<span class="ui-li-count">').text(entry.where));
-
+				// handle tags in event description
+				var tags = (entry.content||'').replace(/\\n/,'\n').match(/@(.+?)\W+(.*)/igm) || [];
+				$.each(tags, function(i, tag) {
+					// parse tag
+					tag = tag.match(/@(.+?)\W+(.*)/i);
+					// prepend # if color is hexadecimal number of 3 or 6 digits
+					if(tag[1] == 'color' && /^([0-9a-f]{3}){1,2}\W*$/i.test(tag[2]))
+						tag[2] = '#'+tag[2];
+					// apply tag to events properties
+					entry[tag[1]] = tag[2];
 				});
 
-				ul.trigger('prepare').listview('refresh').trigger('redraw');
-			})
-			.error(notify.json);
+				// amend icon path/filename
+				if(entry.icon) {
+					// add default path if icon has no path
+					if(entry.icon.indexOf('/') == -1)
+						entry.icon = 'icons/ws/'+entry.icon;
+					// add svg suffix if icon has no suffix
+					if(entry.icon.indexOf('.') == -1)
+						entry.icon = entry.icon+'.svg';
+				}
+				
+				// add entry
+				var a = $('<a>').append(
+					$('<img class="icon">').css('background', entry.color || 'transparent').attr('src', entry.icon || 'pages/base/pics/trans.png')
+				).append(
+					$('<div class="color">').css('background', calcolors[(entry.calendarname||'').toLowerCase()] || entry.calendarcolor || node.attr('data-color').explode()[0] || '#666666')
+				).append(
+					$('<h3>').text(entry.title)
+				).append(
+					$('<p>').text(period)
+				).appendTo(
+				 $('<li data-icon="false">').appendTo(ul)
+				);
+				if(entry.link)
+					a.attr('href', entry.link);
+				if(entry.where)
+					a.append($('<span class="ui-li-count">').text(entry.where));
+
+			});
+
+			ul.trigger('prepare').listview('refresh').trigger('redraw');
+		},
+
+		'repeat': function (event) {
+			widget.getUrlData($(this));
 		}
+
 	});
 
 	// ----- calendar.waste -------------------------------------------------------
 	$(bevent.target).find('div[data-widget="calendar.waste"]').on( {
-		'repeat': function(event) {
+		'update': function(event, response) {
+			var data = response[0];
 			var node = $(this);
 
 			var morgen = new Date();
@@ -1068,59 +1074,62 @@ $(document).on('pagecreate', function (bevent, bdata) {
 			var spalte = 0;
 			var muell_html = "";//<table class ='ui-btn-up-a' style='width:100%;text-align:center;overflow:hidden;'><tr>";
 
-			$.getJSON('lib/calendar/service/'+node.attr('data-service')+'.php?count='+node.attr('data-count')+'&calendar='+node.attr('data-calendar'), function (data) {
+			$.each(data, function(index, entry) {
+				// "pseudo clone" entry to not mute original object in widget.buffer
+				entry = Object.create(entry);
 
-				$.each(data, function(index, entry) {
-					// parse start as Date
-					if(isNaN(entry.start)) // legacy: start in format 'y-m-d H:i:s'
-						entry.start = new Date(Date.parse('20' + entry.start));
-					else // start as timestamp
-						entry.start = new Date(entry.start*1000);
+				// parse start as Date
+				if(isNaN(entry.start)) // legacy: start in format 'y-m-d H:i:s'
+					entry.start = new Date(Date.parse('20' + entry.start));
+				else // start as timestamp
+					entry.start = new Date(entry.start*1000);
 
-					// handle calendar_event_format in lang.ini
-					$.each(sv_lang.calendar_event_format, function(pattern, attributes) {
-						if(entry.title.toLowerCase().indexOf(pattern.toLowerCase()) > -1) { // event title contains pattern
-							// set each defined property
-							$.each(attributes, function(prop, val) {
-								entry[prop] = val;
-							});
-						}
-					});
-
-					// handle tags in event description
-					var tags = (entry.content||'').replace(/\\n/,'\n').match(/@(.+?)\W+(.*)/igm) || [];
-					$.each(tags, function(i, tag) {
-						// parse tag
-						tag = tag.match(/@(.+?)\W+(.*)/i);
-						// prepend # if color is hexadecimal number of 3 or 6 digits
-						if(tag[1] == 'color' && /^([0-9a-f]{3}){1,2}\W*$/i.test(tag[2]))
-							tag[2] = '#'+tag[2];
-						// apply tag to events properties
-						entry[tag[1]] = tag[2];
-					});
-
-					entry.icon = "icons/ws/message_garbage_2.svg";
-
-					muell_html += '<div style="float: left; width: ' + Math.floor(100 / Number(node.attr('data-count'))) + '%;">';
-					muell_html += '<div style="margin: 0 1px; ';
-					if (entry.start < morgen)
-						muell_html += 'border-bottom: red 8px inset; overflow: hidden;';
-					else if (entry.start < uebermorgen)
-						muell_html += 'border-bottom: orange 8px inset; overflow: hidden;';
-					muell_html += '">'
-          muell_html += '<img class="icon icon1" src="' + entry.icon + '" style="width: 100%; height: 100%; fill: ' + entry.color + '; stroke: ' + entry.color + '" />';
-					muell_html += '<div style="font-size: 0.9em;">' + entry.start.transUnit('D') + ', ' + entry.start.transUnit('day') + '</div>'
-					muell_html += '</div>';
-					muell_html += '</div>';
-
+				// handle calendar_event_format in lang.ini
+				$.each(sv_lang.calendar_event_format, function(pattern, attributes) {
+					if(entry.title.toLowerCase().indexOf(pattern.toLowerCase()) > -1) { // event title contains pattern
+						// set each defined property
+						$.each(attributes, function(prop, val) {
+							entry[prop] = val;
+						});
+					}
 				});
 
-				node.find('div').html(muell_html);
-				fx.init(); // load svg inline
+				// handle tags in event description
+				var tags = (entry.content||'').replace(/\\n/,'\n').match(/@(.+?)\W+(.*)/igm) || [];
+				$.each(tags, function(i, tag) {
+					// parse tag
+					tag = tag.match(/@(.+?)\W+(.*)/i);
+					// prepend # if color is hexadecimal number of 3 or 6 digits
+					if(tag[1] == 'color' && /^([0-9a-f]{3}){1,2}\W*$/i.test(tag[2]))
+						tag[2] = '#'+tag[2];
+					// apply tag to events properties
+					entry[tag[1]] = tag[2];
+				});
 
-			})
-			.error(notify.json);
+				entry.icon = "icons/ws/message_garbage_2.svg";
+
+				muell_html += '<div style="float: left; width: ' + Math.floor(100 / Number(node.attr('data-count'))) + '%;">';
+				muell_html += '<div style="margin: 0 1px; ';
+				if (entry.start < morgen)
+					muell_html += 'border-bottom: red 8px inset; overflow: hidden;';
+				else if (entry.start < uebermorgen)
+					muell_html += 'border-bottom: orange 8px inset; overflow: hidden;';
+				muell_html += '">'
+				muell_html += '<img class="icon icon1" src="' + entry.icon + '" style="width: 100%; height: 100%; fill: ' + entry.color + '; stroke: ' + entry.color + '" />';
+				muell_html += '<div style="font-size: 0.9em;">' + entry.start.transUnit('D') + ', ' + entry.start.transUnit('day') + '</div>'
+				muell_html += '</div>';
+				muell_html += '</div>';
+
+			});
+
+			node.find('div').html(muell_html);
+			fx.init(); // load svg inline
+		},
+
+		'repeat': function (event) {
+			widget.getUrlData($(this));
 		}
+
 	});
 
 // ----- d e v i c e ----------------------------------------------------------
