9b524cfb934ca5f835b8b8323749801fba7a2074
smaiLee <stefan@stwidmer.ch>
2018-03-07 15:57:03 +0100

    fix servertime in clock widgets

--- widgets/clock.js
+++ widgets/clock.js
@@ -1,14 +1,54 @@
+$.widget("sv.clock", $.sv.widget, {
+
+	options: {
+		"servertime-url": ""
+	},
+
+	_init: function() {
+		this._super();
+		// init servertime offset on all clocks
+		if(window.servertimeoffset === undefined && this.options["servertime-url"] != '') {
+			if(window.servertimelisteners)
+				window.servertimelisteners.push(this);
+			else {
+				window.servertimelisteners = [this];
+				var localtime = Date.now();
+				$.ajax({
+					url: this.options["servertime-url"],
+					cache: false
+				}).done(function(resp) {
+					var servertime = Number(resp) * 1000;
+					// use average of start and end request timestamp and make it local time
+					localtime = localtime / 2 + Date.now() / 2;
+					window.servertimeoffset = servertime - localtime;
+					//$(bevent.target).find('[data-servertime-url]').trigger('init').trigger('repeat');
+					while(window.servertimelisteners.length > 0)
+						window.servertimelisteners.shift()._init();
+					window.servertimelisteners = null;
+				});
+			}
+		}
+	},
+
+});
+
+
 // ----- clock.digiclock ------------------------------------------------------
-$.widget("sv.clock_digiclock", $.sv.widget, {
+$.widget("sv.clock_digiclock", $.sv.clock, {
 
 	initSelector: 'div.digiclock[data-widget="clock.digiclock"]',
 
-	_create: function() {
+	options: {
+		"service-url": ""
+	},
+
+	_init: function() {
 		this._super();
-		this.element.digiclock({ svrOffset: window.servertimeoffset || 0 });		
+		this.element.digiclock({ svrOffset: window.servertimeoffset || 0 });
+		var that = this;
 		this.element.find('div.digiweather[data-widget="clock.digiclock"]').each(function() {
 			var node = $(this);
-			$.getJSON($(this).attr("data-service-url"), function (data) {
+			$.getJSON(that.options["service-url"], function (data) {
 				node.find('img').attr('src', 'lib/weather/pics/' + data.current.icon + '.png').attr('alt', data.current.icon);
 				node.find('.city').html(data.city);
 				node.find('.cond').html(data.current.conditions);
@@ -22,20 +62,24 @@ $.widget("sv.clock_digiclock", $.sv.widget, {
 
 
 // ----- clock.iconclock ------------------------------------------------------
-$.widget("sv.clock_iconclock", $.sv.widget, {
+$.widget("sv.clock_iconclock", $.sv.clock, {
 
 	initSelector: 'span[data-widget="clock.iconclock"]',
 
 	_repeat: function() {
 		var now = new Date(Date.now() - (window.servertimeoffset || 0));
 		var minutes = Math.floor(now.getHours()*60 + now.getMinutes());
-		this.element.find('svg').trigger('update', [[minutes]]);
+		var icon = this.element.find('svg');
+		if(icon.is(':sv-icon_clock'))
+			icon.icon_clock('update', [minutes]);
+		else
+			icon.on("icon_clockcreate", function( event, ui ) { icon.icon_clock('update', [minutes]); } );
 	},
 
 });
 
 // ----- clock.miniclock ------------------------------------------------------
-$.widget("sv.clock_miniclock", $.sv.widget, {
+$.widget("sv.clock_miniclock", $.sv.clock, {
 
 	initSelector: 'span[data-widget="clock.miniclock"]',
 
@@ -49,26 +93,3 @@ $.widget("sv.clock_miniclock", $.sv.widget, {
 	},
 
 });
-
-//TODO
-/*
-	// init servertime offset on all clocks
-	$(bevent.target).find('[data-servertime-url]:first').each(function() { // init
-		if(window.servertimeoffset === undefined) {
-			var localtime = Date.now();
-			$.ajax({
-				url: $(this).attr("data-servertime-url"),
-				cache: false
-			}).done(function(resp) {
-				var servertime = Number(resp) * 1000;
-				// use average of start and end request timestamp and make it local time
-				localtime = localtime / 2 + Date.now() / 2;
-				window.servertimeoffset = servertime - localtime;
-				$(bevent.target).find('[data-servertime-url]').trigger('init').trigger('repeat');
-			});
-		}
-		else {
-			$(bevent.target).find('[data-servertime-url]').trigger('init');
-		}
-	});
-*/
