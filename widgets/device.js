2c192e64c7b127fdc6ee8f390e9c54441ee93e1a
Stefan <smaiLee@users.noreply.github.com>
2019-01-08 17:38:49 +0100

    removed scatter
    Co-Authored-By: onkelandy <onkelandy@hotmail.com>

--- widgets/device.js
+++ widgets/device.js
@@ -1456,7 +1456,6 @@ $.widget("sv.device_uzsugraph", $.sv.device_uzsu, {
     chart.get('active').setData(data, false, null, false);
     chart.get('active').update({
       // type: this._uzsudata.interpolation.type == 'cubic' ? 'spline' : 'line',
-      type: 'scatter',
       step: this._uzsudata.interpolation.type != 'cubic' && this._uzsudata.interpolation.type != 'linear' ? 'left' : false,
     }, false);
 
