94bb45de1ae1698f1fec49e8b82ed9e0459acf5d
smaiLee <stefan@stwidmer.ch>
2019-11-01 13:33:42 +0100

    make update check configurable, remove system hash
    resolves #235

--- lib/base/check_update.php
+++ lib/base/check_update.php
@@ -16,12 +16,7 @@ require_once '../../lib/includes.php';
 $request = array_merge($_GET, $_POST);
 
 // get contents
-$url = "http://smartvisu.de/download/check.php?local=".(isset($request["local"]) ? $request["local"] : config_version);
-
-exec('cat /sys/class/net/eth0/address', $out, $error);
-if ($error)
-	$out[0] = $_SERVER['SERVER_SOFTWARE'].$_SERVER['SERVER_NAME'].$_SERVER['SERVER_ADDR'];
-$url .= "&hash=".md5($out[0]).substr(config_driver, 0, 1);
+$url = "https://smartvisu.de/download/check.php?local=".(isset($request["local"]) ? $request["local"] : config_version);
 
 $data = json_decode(file_get_contents($url, false));
 
