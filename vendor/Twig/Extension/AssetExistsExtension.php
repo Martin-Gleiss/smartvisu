f4bfd8df8bbba2ec6969cfa1366fdd6f57725bee
onkelandy <onkelandy@hotmail.com>
2020-03-13 23:13:00 +0100

    improve AssetExist to check all possible paths and work in PHP5

--- vendor/Twig/Extension/AssetExistsExtension.php
+++ vendor/Twig/Extension/AssetExistsExtension.php
@@ -12,16 +12,21 @@ class Twig_Extension_AssetExistsExtension extends Twig_Extension
 
     public function asset_exists($path)
     {
-        $webRoot = realpath(dirname(__DIR__, 3));
-        $toCheck = $webRoot . '/widgets/' . $path;
+        $webRoot = realpath(dirname(dirname(dirname(__DIR__))));
+        $path = end(explode('widgets/', $path));
+        $toCheck1 = $webRoot . '/widgets/' . $path;
+        $toCheck2 = $webRoot . '/dropins/widgets/' . $path;
+        $toCheck3 = $webRoot . '/pages/' . parse_ini_file($webRoot . '/config.ini')['pages'] . '/widgets/' . $path;
+        $toCheck4 = $webRoot . '/pages/' . end(explode('pages=', $_SERVER['REQUEST_URI'])) . '/widgets/' . $path;
+
         // check if the file exists
-        if (!is_file($toCheck))
+        if (!is_file($toCheck1) && !is_file($toCheck2) && !is_file($toCheck3) && !is_file($toCheck4))
         {
             return false;
         }
 
         // check if file is well contained in web/ directory (prevents ../ in paths)
-        if (strncmp($webRoot, $toCheck, strlen($webRoot)) !== 0)
+        if (strncmp($webRoot, $toCheck1, strlen($webRoot)) !== 0 && strncmp($webRoot, $toCheck2, strlen($webRoot)) !== 0 && strncmp($webRoot, $toCheck3, strlen($webRoot)) !== 0 && strncmp($webRoot, $toCheck4, strlen($webRoot)) !== 0)
         {
             return false;
         }
