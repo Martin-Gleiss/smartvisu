7c29ccbd4c10833d53028f04efa2f51ae970b12d
onkelandy <onkelandy@hotmail.com>
2020-03-14 23:01:35 +0100

    write to console

--- vendor/Twig/Extension/AssetExistsExtension.php
+++ vendor/Twig/Extension/AssetExistsExtension.php
@@ -10,6 +10,17 @@ class Twig_Extension_AssetExistsExtension extends Twig_Extension
         );
     }
 
+    function debug_to_console($data) {
+
+    // Buffering to solve problems frameworks, like header() in this and not a solid return.
+    ob_start();
+    $data = 'PHP: ' . $data;
+    $output = 'console.log(' . json_encode($data) . ');';
+    $output = sprintf('<script>%s</script>', $output);
+
+    echo $output;
+    }
+
     public function asset_exists($path)
     {
         $webRoot = realpath(dirname(dirname(dirname(__DIR__))));
@@ -23,12 +34,14 @@ class Twig_Extension_AssetExistsExtension extends Twig_Extension
         // check if the file exists
         if (!is_file($toCheck1) && !is_file($toCheck2) && !is_file($toCheck3) && !is_file($toCheck4))
         {
+            $this->debug_to_console($path . " does not exist in these directories: " . $toCheck1 . ", " . $toCheck2 . ", " . $toCheck3 . ", " . $toCheck4 . ".");
             return false;
         }
 
         // check if file is well contained in web/ directory (prevents ../ in paths)
         if (strncmp($webRoot, $toCheck1, strlen($webRoot)) !== 0 && strncmp($webRoot, $toCheck2, strlen($webRoot)) !== 0 && strncmp($webRoot, $toCheck3, strlen($webRoot)) !== 0 && strncmp($webRoot, $toCheck4, strlen($webRoot)) !== 0)
         {
+            $this->debug_to_console($path . " does not exist in these directories: " . $toCheck1 . ", " . $toCheck2 . ", " . $toCheck3 . ", " . $toCheck4 . ".");
             return false;
         }
         return true;
