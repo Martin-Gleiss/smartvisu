5adec9af25f5578079b8c2a6f1277f0c58aaa504
PatrikG <40170469+PatrikG8@users.noreply.github.com>
2020-03-17 22:31:16 +0100

    Neuer Treiber für openHAB 2
    Unterstützt alle Widgets, Devices und Plots von smartVISU 2.8 und alle Items aus openHAB 2, außer Color (HSB/V) ohne besondere Notationen. Nur bei Devices muss für alle Data-Items dasselbe openHAB2-Item angegeben werden.

--- driver/io_openhab2.js
+++ driver/io_openhab2.js
@@ -1,11 +1,12 @@
 /**
  * -----------------------------------------------------------------------------
  * @package     smartVISU
- * @author      Martin Gleiß
- * @copyright   2012 - 2015
+ * @author      Patrik Germann
+ * @copyright   2020
  * @license     GPL [http://www.gnu.de]
  * -----------------------------------------------------------------------------
  */
+
 
 /**
  * Class for controlling all communication with a connected system. There are
@@ -13,342 +14,268 @@
  */
 var io = {
 
-    // the adress
-    address : '',
+	// the URL
+	url: '',
 
-    // the port
-    port : '',
 
-    // -----------------------------------------------------------------------------
-    // P U B L I C F U N C T I O N S
-    // -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
+	// P U B L I C   F U N C T I O N S
+	// -----------------------------------------------------------------------------
 
-    /**
-     * Does a read-request and adds the result to the buffer
-     * 
-     * @param the
-     *            item
-     */
-    read : function(item) {
-        io.get(item);
-    },
+	/**
+	 * Does a read-request and adds the result to the buffer
+	 *
+	 * @param      the item
+	 */
+	read: function (item) {
+		$.ajax({
+            url : io.url + 'items/' + item + '/state',
+            type : "GET",
+            async : true,
+            cache : false
+		}).done(function(val) {
+            widget.update(item, io.transval[val] ? io.transval[val] : val);
+        }).error(notify.json);
+	},
 
-    /**
-     * Does a write-request with a value
-     * 
-     * @param the
-     *            item
-     * @param the
-     *            value
-     */
-    write : function(item, val) {
-        io.put(item, val);
-    },
+	/**
+	 * Does a write-request with a value
+	 *
+	 * @param      the item
+	 * @param      the value
+	 */
+	write: function (item, val) {
+		if (val == 0 || val == 1) {
+			var transval = new Array();
+			var elementID = $(event.currentTarget).attr('id');
+			switch (io.itemType[item]) {
+				case "Switch":
+					transval[0] = "OFF";
+					transval[1] = "ON";
+				break;
+				case "Contact":
+					transval[0] = "CLOSED";
+					transval[1] = "OPEN";
+				break;
+				case "Dimmer":
+					transval[0] = "OFF";
+					transval[1] = "ON";
+					if (elementID.slice(-4) == 'plus' || elementID.slice(-5) == 'minus') {
+						transval[0] = "DECREASE";
+						transval[1] = "INCREASE";
+					}
+				break;
+				case "Rollershutter":
+					transval[0] = "UP";
+					transval[1] = "DOWN";
+					if (elementID.slice(-2) == 'up' || elementID.slice(-4) == 'down') {
+						transval[0] = "UP";
+						transval[1] = "DOWN";
+					} else if (elementID.slice(-5) == 'start' || elementID.slice(-4) == 'stop') {
+						transval[0] = "STOP";
+						transval[1] = "START";
+					} else if (elementID.slice(-4) == 'plus' || elementID.slice(-5) == 'minus') {
+						transval[0] = "DECREASE";
+						transval[1] = "INCREASE";
+					}
+				break;
+				case "Player":
+					transval[0] = "PAUSE";
+					transval[1] = "PLAY";
+					if (elementID.slice(-4) == 'stop' || elementID.slice(-4) == 'play') {
+						transval[0] = "PAUSE";
+						transval[1] = "PLAY";
+					} else if (elementID.slice(-4) == 'prev' || elementID.slice(-4) == 'next') {
+						transval[0] = "PREVIOUS";
+						transval[1] = "NEXT";
+					} else if (elementID.slice(-3) == 'rew' || elementID.slice(-2) == 'ff') {
+						transval[0] = "REWIND";
+						transval[1] = "FASTFORWARD";
+					}
+				break;
+			default:
+			}
+			if (transval[val]) {
+				val = transval[val];
+			}
+		}
+		
+		
+		$.ajax({
+            url			: io.url + 'items/' + item,
+            data		: val.toString(),
+            method		: "POST",
+            contentType	: "text/plain",
+            cache		: false
+		}).error(notify.json);
+	},
 
-    /**
-     * Trigger a logic
-     * 
-     * @param the
-     *            logic
-     * @param the
-     *            value
-     */
-    trigger : function(name, val) {
-        // not supported
-    },
+	/**
+	 * Trigger a logic
+	 *
+	 * @param      the logic
+	 * @param      the value
+	 */
+	trigger: function (name, val) {
+		
+	},
 
-    /**
-     * Initializion of the driver
-     * 
-     * @param the
-     *            ip or url to the system (optional)
-     * @param the
-     *            port on which the connection should be made (optional)
-     */
-    init : function(address, port) {
-        io.address = address;
-        io.port = port;
-        io.stop();
-    },
+	/**
+	 * Initializion of the driver
+	 *
+	 * @param      the ip or url to the system (optional)
+	 * @param      the port on which the connection should be made (optional)
+	 */
+	init: function (address, port) {
+		io.url = "http://" + address + (port ? ":" + port : '') + "/rest/";
+	},
 
-    /**
-     * Lets the driver work
-     */
-    run : function(realtime) {
-        // old items
-        widget.refresh();
+	/**
+	 * Lets the driver work
+	 */
+	run: function (realtime) {
+		io.stop();
 
-        // new items
-        io.all();
-        io.allPlots();
-
-        // run polling
-        if (realtime) {
-            io.start();
+        if (widget.listeners().length) {
+            var items = widget.listeners();
+            for (var i = 0; i < widget.listeners().length; i++) {
+				$.ajax({
+					url		: io.url + 'items/' + items[i],
+					type	: "GET",
+					async	: true,
+					cache	: false
+				}).done(function(ohItem) {
+					var item = ohItem.name;
+					io.itemType[item] = ohItem.type;
+					widget.update(item, io.transval[ohItem.state] ? io.transval[ohItem.state] : ohItem.state);
+				}).error(notify.json);
+            }
         }
-    },
+		
+        io.plot.init();
 
-    // -----------------------------------------------------------------------------
-    // C O M M U N I C A T I O N F U N C T I O N S
-    // -----------------------------------------------------------------------------
-    // The function in this paragraph may be changed. They are all private and
-    // are
-    // only be called from the public functions above. You may add or delete
-    // some
-    // to fit your requirements and your connected system.
+		if (realtime) {
+			io.start();
+		}
+	},
 
-    plotItems: Array(),
-    
+
+	// -----------------------------------------------------------------------------
+	// C O M M U N I C A T I O N   F U N C T I O N S
+	// -----------------------------------------------------------------------------
+	// The functions in this paragraph may be changed. They are all private and are
+	// only be called from the public functions above. You may add or delete some
+	// to fit your requirements and your connected system.
+
+	transval : {
+		null		: "0",
+		"NULL"		: "0",
+		"OFF"		: "0", "ON"			: "1",
+		"CLOSED"	: "0", "OPEN"		: "1",
+		"PAUSE"		: "0", "PLAY"		: "1",
+		"REWIND"	: "0", "FASTFORWARD": "1"
+	},
+	itemType: new Array(),
+	eventListener: false,
+
     /**
      * Start the real-time values. Can only be started once
      */
-    start : function() {
-        var me = this;
-        var eventSource = new EventSource("http://" + io.address + ":" + io.port + "/rest/events?topics=smarthome/items/*/state");
+	start: function () {
+		io.eventListener = new EventSource(io.url + "events?topics=smarthome/items/*/statechanged");
+		io.eventListener.onmessage = function(message) {
+			var event = JSON.parse(message.data);
+			if (event.type === 'ItemStateChangedEvent') {
+				var item = event.topic.split('/')[2];
+				if (widget.listeners().includes(item)) {
+					var val = JSON.parse(event.payload).value
+					widget.update(item, io.transval[val] ? io.transval[val] : val);
+				}
+				if (io.plot.listeners[item] && Date.now() - io.plot.items[io.plot.listeners[item]] > io.plot.timer * 1000) {
+					io.plot.refresh(io.plot.listeners[item]);
+				}
+			}
+		}
+	},
 
-        eventSource.addEventListener('message', function(eventPayload) {
-
-            var event = JSON.parse(eventPayload.data);
-            //console.log(event);
-
-            if (event.type === 'ItemStateEvent') {
-                var payload = JSON.parse(event.payload);
-
-                var ohItem = event.topic.split('/')[2];
-                console.log("ItemStateEvent for " + ohItem);
-
-                var item = io.getItemFromOHItem(ohItem);
-
-                if (item)
-                    widget.update(item, io.convertFromOH(item, payload.value));
-                
-                // Check if this is a plot item
-                me.plotItems.forEach(function(plotItem) {
-                    var pt = plotItem.split('.');
-                    var ptOhItem = io.getOHItemFromItem(pt[0]);
-                    
-                    if (ptOhItem == ohItem) {
-                        console.log("Updating plot item " + plotItem);
-                        var data = [[new Date().getTime(), parseFloat(io.convertFromOH(item, payload.value))]];
-                        console.log(data);
-                        widget.update(plotItem, data);
-                    }
-                });
-            }
-        });
-
-    },
 
     /**
      * Stop the real-time values
      */
     stop : function() {
+		if (io.eventListener.readyState == 0 || io.eventListener.readyState == 1) {
+			io.eventListener.close();
+		}
     },
 
-    /**
-     * Read a specific item from bus and add it to the buffer
-     */
-    get : function(item) {
-        var url = "http://" + io.address + ":" + io.port + "/rest/items/" + io.getOHItemFromItem(item) + "/state";
+	
+	plot : {
+		items : new Array(),
+		listeners : new Array(),
+		timer : 60,
+		
+		init : function() {
+			io.plot.listeners = new Array();
+			widget.plot().each(function(idx) {
+				var items = widget.explode($(this).attr('data-item'));
+				for (var i = 0; i < items.length; i++) {
+					var plotItem = items[i];
+					var pt = plotItem.split('.');
+					if (!io.plot.items[plotItem] && (pt instanceof Array) && widget.checkseries(plotItem)) {
+						if (pt[3] == 'now') {
+							io.plot.listeners[pt[0]] = plotItem;
+						}
+						io.plot.refresh(plotItem);
+						io.plot.items[plotItem] = true;
+					}
+				}
+			});
+		},
 
-        $.ajax({
-            url : url,
-            type : "GET",
-            async : true,
-            cache : false
-        }).done(function(response) {
-            widget.update(item, response);
-        }).error(notify.json);
-    },
 
-    /**
-     * Write a value to bus
-     */
-    put : function(item, val) {
-        var timer_run = io.timer_run;
-        var url = "http://" + io.address + ":" + io.port + "/rest/items/" + io.getOHItemFromItem(item);
+		refresh : function(plotItem) {
+			var pt = plotItem.split('.');
+			var item = pt[0];
+			var tmin = new Date().duration(pt[2]);
+			var tmax = pt[3];
+		
+			var starttime = new Date(Date.now() - tmin);
+			var url = io.url + "persistence/items/" + item + "?starttime=" + starttime.toISOString();
+			
+			if (tmax != 'now') {
+				tmax = new Date().duration(pt[3]);
+				var endtime = new Date(new Date() - tmax);
+				url += "&endtime=" + endtime.toISOString();
+			}
 
-        io.stop();
-        $.ajax({
-            url : url,
-            data : io.convertToOH(item, val),
-            method : "POST",
-            contentType : "text/plain",
-            cache : false
-        }).success(function() {
-            widget.update(item, val);
-            if (timer_run) {
-                io.start();
-            }
-        }).error(notify.json);
-    },
-
-    convertToOH : function(item, val) {
-        var type = io.getTypeFromItem(item);
-        var ohVal = val;
-
-        switch (type) {
-        case "Switch":
-            if (val == 1)
-                ohVal = "ON";
-            else
-                ohVal = "OFF";
-            break;
-        }
-
-        return ohVal;
-    },
-
-    convertFromOH : function(item, ohVal) {
-        var type = io.getTypeFromItem(item);
-        var val = ohVal;
-
-        switch (type) {
-        case "Switch":
-            if (ohVal == "ON")
-                val = 1;
-            else
-                val = 0;
-            break;
-        }
-
-        return val;
-    },
-
-    getTypeFromItem : function(item) {
-        var itemArray = item.split(":");
-
-        var type = "Unknown";
-
-        if (itemArray.length == 2) {
-            type = itemArray[0]
-        }
-
-        // console.log("Determined item type: " + type);
-
-        return type;
-    },
-
-    getOHItemFromItem : function(item) {
-        var itemArray = item.split(":");
-
-        var ohItem = item;
-
-        if (itemArray.length == 2) {
-            ohItem = itemArray[1]
-        }
-
-        // console.log("Determined Openhab item: " + ohItem);
-
-        return ohItem;
-    },
-
-    getItemFromOHItem : function(ohItem) {
-        // console.log("Searching for oh item: " + ohItem);
-
-        var items = widget.listeners();
-        for (var i = 0; i < items.length; i++) {
-            if (ohItem == io.getOHItemFromItem(items[i])) {
-                // console.log("Determined item: " + items[i]);
-                return items[i];
-            }
-        }
-
-        return false;
-    },
-
-    /**
-     * Reads all values from bus and refreshes the pages
-     */
-    all : function() {
-        var items = '';
-
-        // only if anyone listens
-        if (widget.listeners().length) {
-            // prepare url
-            var item = widget.listeners();
-            for (var i = 0; i < widget.listeners().length; i++) {
-                items += item[i] + ',';
-            }
-            items = items.substr(0, items.length - 1);
-
-            var url = "http://" + io.address + ":" + io.port + "/rest/items";
-
-            $.ajax({
-                url : url,
-                type : 'GET',
-                dataType : 'json',
-                async : true,
-                cache : false
-            }).done(function(response) {
-                $.each(response, function(index, ohItem) {
-                    var item = io.getItemFromOHItem(ohItem.name);
-
-                    if (item)
-                        widget.update(item, io.convertFromOH(item, ohItem.state));
-                })
-            }).error(notify.json);
-        }
-    },
-
-    allPlots : function() {
-        console.log("Updating all plots");
-        
-        var me = this;
-        
-        var unique = Array();
-        
-        widget.plot().each(function(idx) {
-            var items = widget.explode($(this).attr('data-item'));
-            for (var i = 0; i < items.length; i++) {
-
-                var pt = items[i].split('.');
-
-                if (!unique[items[i]] && !widget.get(items[i]) && (pt instanceof Array) && widget.checkseries(items[i])) {
-                    var item = items[i];
-                    console.log(item);
-                    
-                    var ohItem = io.getOHItemFromItem(pt[0]);
-                    
-                    var duration = new Date().duration(pt[2]);
-                    console.log(duration);
-                    
-                    // For OpenHAB it seems to be required to send a date in the correct timezone
-                    // as it seems to ignore the timezone offset
-                    var tzoffset = (new Date()).getTimezoneOffset() * 60000;
-                    var starttime = new Date(new Date() - duration - tzoffset).toISOString().slice(0, -1);
-                    console.log(starttime);
-
-                    console.log("Updating plot for " + ohItem);
-
-                    var url = "http://" + io.address + ":" + io.port + "/rest/persistence/" + ohItem + "?servicename=rrd4j&starttime=" + starttime;
-
-                    $.ajax({
-                        url : url,
-                        type : 'GET',
-                        dataType : 'json',
-                        async : true,
-                        cache : false
-                    }).done(function(response) {
-                        var valArray = [];
-
-                        $.each(response.data, function(index, dataVal) {
-                            valArray.push([ dataVal.time, parseFloat(dataVal.state) ]);
-                        })
-
-                        valArray.sort(function(a, b) {
-                           return a[0] - b[0]; 
-                        });
-                        
-                        console.log(valArray);
-                        widget.update(item, valArray);
-                    }).error(notify.json);
-
-                    unique[items[i]] = 1;
-                    me.plotItems.push(items[i]);
-                }
-            }
-        });
-    }
-
-};
+			$.ajax({
+				url : url,
+				type : 'GET',
+				dataType : 'json',
+				async : false,
+				cache : false
+			}).done(function(persistence) {
+				var plotData = new Array();
+				if (persistence.data.length > 0) {
+					$.each(persistence.data, function(key, data) {
+						var val = io.transval[data.state] ? io.transval[data.state] : data.state
+						if (isNaN(val)) {
+							val = 0;
+						} else if (Number(val) == val && val % 1 !== 0) { //isFloat
+							val = parseFloat(val).toFixed(2);
+						}
+						plotData.push([data.time, parseFloat(val)]);
+					})
+					plotData.sort(function(a, b) {
+						return a[0] - b[0];
+					});
+				} else {
+					plotData.push([Date.parse(starttime), 0]);
+					plotData.push([endtime ? Date.parse(endtime) : Date.now(), 0])
+				}
+				io.plot.items[plotItem] = Date.now();
+				widget.update(plotItem, plotData);
+			}).error(notify.json);
+		}
+	}
+}
