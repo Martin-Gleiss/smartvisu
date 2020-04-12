332b4b7172140e71d40095478cdd59d69a27f541
PatrikG <40170469+PatrikG8@users.noreply.github.com>
2020-04-12 15:16:35 +0200

    change messagetext

--- lib/base/check_config.php
+++ lib/base/check_config.php
@@ -17,12 +17,12 @@ $request = array_merge($_GET, $_POST);
 
 if ((!is_file(const_path.'config.ini') && is_writeable(const_path)) || (is_file(const_path.'config.ini') && is_writeable(const_path.'config.ini')))
 {
-	$ret = array('icon' => 'message_ok.svg', 'text' => "'config.ini' file is writable or can be created");
+	$ret = array('icon' => 'message_ok.svg', 'text' => "'config.ini' file is writeable or can be created");
 }
 else
 {
 	header("HTTP/1.0 600 smartVISU Config Error");
-	$ret = array('icon' => 'message_attention.svg', 'text' => "'".const_path."config.ini' file isn't writable or can't be created!");
+	$ret = array('icon' => 'message_attention.svg', 'text' => "'".const_path."config.ini' file isn't writeable or can't be created!");
 }
 
 echo json_encode($ret);
