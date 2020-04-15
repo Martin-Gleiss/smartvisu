ac637640edc1b964306a0ee80e62f830b6cb9ca0
Hasenradball <mail@frankhaefele.de>
2020-04-15 11:21:47 +0200

    # update fritz!box_TR-064.php - last working version with `get call list` on Fritz!Box 7490

--- lib/phone/service/fritz!box_TR-064.php
+++ lib/phone/service/fritz!box_TR-064.php
@@ -23,12 +23,6 @@ class phone_fritzbox_TR064 extends phone
     private $max_calls_to_fetch;
     private $challenge;
     private $call_list_url;
-    private $context_ssl = array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-    );
-    
     public function __construct($http_vals)
     {
         parent::init($http_vals);
@@ -37,8 +31,6 @@ class phone_fritzbox_TR064 extends phone
         // use some default user if only password is set on smartvisu 
         if (strlen($this->user) == 0 && strlen($this->pass) > 0)
             $this->user = 'admin';
-        if (strlen($this->port) == 0)
-            $this->port = '49000';
     }
     /**
      * DoSOAPCall: issues a http post to the phone system on port 49000
@@ -53,12 +45,9 @@ class phone_fritzbox_TR064 extends phone
                 'method' => 'POST',
                 'header' => implode("\r\n", $header),
                 'content' => $content
-            ),
-            'ssl' => $this->context_ssl
+            )
         );
-        $protocol = ($this->port == '49443') ? 'https' : 'http';
-        $baseurl = strpos($this->server, '://') === FALSE ? $protocol."://".$this->server : $this->server;
-        $url      = $baseurl.":".$this->port."/upnp/control/x_contact";
+        $url      = 'http://' . $this->server . ':49000/upnp/control/x_contact';
         
         return (file_get_contents($url, false, stream_context_create($context)));        
     }
@@ -133,7 +122,7 @@ class phone_fritzbox_TR064 extends phone
         $url = $this->call_list_url . '&max=' . $this->max_calls_to_fetch;
         $this->debug($url, "URL for call_list");
         // download xml file and put it to xml parser
-        $GetCallListXml = file_get_contents($url, false, stream_context_create(array('ssl' => $self->context_ssl)));
+        $GetCallListXml = file_get_contents($url);
         $simplexml      = simplexml_load_string($GetCallListXml);
         $this->debug($GetCallListXml, "GetCallListXml");
         /*
