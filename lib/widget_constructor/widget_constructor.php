19e3dcd91cc6c208d997c5c4d443b28f6c1af00f
AndreK <andre.kohler01@googlemail.com>
2020-04-19 19:16:58 +0200

    new feature in Config - widget constructor

---
+++ lib/widget_constructor/widget_constructor.php
@@ -0,0 +1,95 @@
+<?php
+
+
+// ************************************************
+// function to get all the icons
+// ************************************************
+
+function get_icons ($value)
+{
+
+$dirlist = dir('../../icons/ws/');
+$myArray = [];
+while (($item = $dirlist->read()) !== false)
+{
+	array_push($myArray,$item);
+}
+
+$dirlist->close();
+
+
+return json_encode($myArray);
+}
+
+// ********************************************************
+// function to render the Template for the preview inline
+// ********************************************************
+
+function render_inline ($value)
+{
+	$widget = $value["widget"];
+	$myFile = file_get_contents('tmpl_construct_2.html');
+	$myFile = str_replace("%widget%",$widget, $myFile);
+	file_put_contents('../../pages/base/construct.html', $myFile);
+    return $myFile;
+}
+
+// ************************************************************************
+// function to render the Template for the preview outline with auto-Update
+// ************************************************************************
+
+function render_outline ($value)
+{
+	$widget = $value["widget"];
+	$myFile = file_get_contents('tmpl_construct_1.html');
+	$myFile = str_replace("%widget%",$widget, $myFile);
+	file_put_contents('../../pages/base/construct.html', $myFile);
+    return $myFile;
+}
+
+// ************************************************
+// function to render the Template for the preview
+// ************************************************
+function load_widgets ($value)
+{
+	$myFile = file_get_contents('masteritem.json');
+    return $myFile;
+}
+
+// ************************************************
+// function to render the Template for the preview
+// ************************************************
+function get_time_stamp ($value)
+{
+    $ret = filemtime('../../pages/base/construct.html');
+    return $ret;
+}
+
+// ************************************************
+// main
+// ************************************************
+
+$command = $_GET["command"];
+
+switch ($command)
+{
+    case 'render_inline':
+        $ret = render_inline($_GET);
+        break;
+    case 'render_outline':
+        $ret = render_outline($_GET);
+        break;
+    case 'load_widgets':
+        $ret = load_widgets($_GET);
+        break;
+    case 'load_icons':
+        $ret = get_icons($_GET);
+        break;
+    case 'get_time_stamp':
+        $ret = get_time_stamp($_GET);
+        break;
+
+}
+
+echo $ret;
+?>
