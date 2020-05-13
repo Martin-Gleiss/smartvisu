1c2858061e09b299eb0265167872d6aad62a26ba
smartVISU-newstuff <60430485+smartVISU-newstuff@users.noreply.github.com>
2020-05-14 00:10:29 +0200

    update minor version no. to prepare for release

--- version-info.php
+++ version-info.php
@@ -3,5 +3,5 @@ define ('config_visu', 'smartVISU');
 define ('config_version', '2.9');
 define ('config_version_major', '2');
 define ('config_version_minor', '9');
-define ('config_version_revision', 'a');
+define ('config_version_revision', '2');
 ?>
