1ba05430d48b656ec5dc2df0fccc51f867395ce0
wvhn <17801971+wvhn@users.noreply.github.com>
2020-09-02 15:04:43 +0200

    transmit new version format to backend

--- version-info.php
+++ version-info.php
@@ -1,6 +1,6 @@
 <?php
 define ('config_visu', 'smartVISU');
-define ('config_version', '2.9');
+define ('config_version', '2.9');  // deprecated as from ...
 define ('config_version_major', '2');
 define ('config_version_minor', '9');
 define ('config_version_revision', 'b');
