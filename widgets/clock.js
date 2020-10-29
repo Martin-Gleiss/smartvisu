abec57bfc9208bcfea74ef442bf7a0e5f2eb92d0
wvhn <17801971+wvhn@users.noreply.github.com>
2020-10-29 22:15:06 +0100

    add options for timer stop and idle display

--- widgets/clock.js
+++ widgets/clock.js
@@ -113,12 +113,15 @@ $.widget("sv.clock_countdown", $.sv.widget, {
 
 	options: {
 		interval: 0,
+		stopmode: 'item',
+		idletxt: '--:--:--',
 		 	},
 
 	_currentstarttime: 0,
 	_currentduration: 0,
 	_olditem: null,
 	_timer_run: false,
+	_ticker: null,
 
 	_update: function(response) {
 		var item = response[0];
@@ -128,7 +131,7 @@ $.widget("sv.clock_countdown", $.sv.widget, {
 		var itemduration = +new Date().duration(durationitem);  // item duration in milliseconds
 		var interval = +new Date().duration(this.options.interval); // countdown interval in milliseconds
 		
-		console.log('[update] item: ', item, ' _old: ', this._olditem, ' start: ', starttime,' duration: ', itemduration, ' actual: ', actualtime, ' _current: ', this._currentstarttime);
+		// console.log('[update] item: ', item, ' _old: ', this._olditem, ' start: ', starttime,' duration: ', itemduration, ' actual: ', actualtime, ' _current: ', this._currentstarttime);
 		
 		// allow change of duration during running countdown
 		this._currentduration = itemduration;
@@ -139,18 +142,17 @@ $.widget("sv.clock_countdown", $.sv.widget, {
 			// memorize item for abort on change
 			this._olditem = item;
 			countdown(this);
-			ticker = setInterval(countdown, interval, this); 
+			this._ticker = setInterval(countdown, interval, this); 
 			this._timer_run = true;
 			console.log('countdown timer started at ', new Date(starttime).transUnit("H:i:s"), ' for', itemduration/1000, ' seconds');
 		};
 
 		// if item is changed while starttime is unchanged, abort countdown
 		if (item != this._olditem && starttime == this._currentstarttime && this._timer_run == true) {
-			clearInterval(ticker);
-			// this._currentstarttime = 0;
-			this.element.text('--:--:--');
+			clearInterval(this._ticker);
+			this.element.text(this.options.idletxt);
 			this._timer_run = false;
-			console.log('countdown timer stopped at ', new Date().transUnit("H:i:s"));
+			console.log('countdown timer stopped by item at ', new Date().transUnit("H:i:s"));
 		};
 		
 		if (this._currentstarttime > 0) this._currentstarttime = starttime;
@@ -158,7 +160,14 @@ $.widget("sv.clock_countdown", $.sv.widget, {
 		function countdown(that) {
 			var telapsed = Date.now() - that._currentstarttime;
 			var timetogo = that._currentduration - telapsed;   // remaining time in milliseconds
-			that.element.text(timedisplay(timetogo));
+			if (that.options.stopmode == "zero" && timetogo <= 0) {
+				clearInterval(that._ticker);
+				that.element.text(that.options.idletxt);
+				that._timer_run = false;
+				console.log('countdown timer stopped on zero at ', new Date().transUnit("H:i:s"));
+			} else {	
+				that.element.text(timedisplay(timetogo));
+			};			
 		};
 	},
 
