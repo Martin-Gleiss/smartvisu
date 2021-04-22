6f46ef176ddc59e87757479ca3fe246e1bbe187f
wvhn <17801971+wvhn@users.noreply.github.com>
2021-04-22 17:45:48 +0200

    complete review of docstrings
    and docu page fixes for errors in template checker

--- lib/templatechecker/class.Config.php
+++ lib/templatechecker/class.Config.php
@@ -53,7 +53,7 @@ class TemplateCheckerConfig {
 	 * Array containing known button types
 	 * @var array 
 	 */
-	const SmartvisuButtonTypes = array('micro', 'mini', 'midi', 'icon', 'text', 'btn-micro', 'btn-mini', 'btn-midi');
+	const SmartvisuButtonTypes = array('micro', 'mini', 'midi', 'icon');
 
 	/**
 	 * Array containing valid interval values for duration format
