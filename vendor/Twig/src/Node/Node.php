734b27010b7e3ea7ce32ddd5e0c18b22bd746ea7
wvhn <17801971+wvhn@users.noreply.github.com>
2022-09-21 23:20:41 +0200

    twig: improve php 8.1 compatibility

--- vendor/Twig/src/Node/Node.php
+++ vendor/Twig/src/Node/Node.php
@@ -210,6 +210,7 @@ class Node implements \Twig_NodeInterface
     /**
      * @return int
      */
+	#[\ReturnTypeWillChange]
     public function count()
     {
         return \count($this->nodes);
@@ -218,7 +219,7 @@ class Node implements \Twig_NodeInterface
     /**
      * @return \Traversable
      */
-    public function getIterator()
+    public function getIterator(): \Traversable
     {
         return new \ArrayIterator($this->nodes);
     }
