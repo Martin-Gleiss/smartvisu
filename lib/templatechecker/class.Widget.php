b56c178425061f24594183eb294b7a2b10f33c46
wvhn <17801971+wvhn@users.noreply.github.com>
2024-02-23 23:04:56 +0100

    templatechecker fixes

--- lib/templatechecker/class.Widget.php
+++ lib/templatechecker/class.Widget.php
@@ -203,7 +203,7 @@ class Widget {
 					}
 				}
 				else
-					$currentParam = trim($currentParam, " \t\n\r\0\x0B'");
+					$currentParam = trim($currentParam, " \t\n\r\0\x0B'\"");
 				
 				$paramArray[] = $currentParam;
 				if ($topLevel == 1) {
@@ -231,7 +231,7 @@ class Widget {
 					}
 			}
 			else
-				$currentParam = trim($currentParam, " \t\n\r\0\x0B'");
+				$currentParam = trim($currentParam, " \t\n\r\0\x0B'\"");
 
 			$paramArray[] = $currentParam;
 			if ($topLevel == 1) 
