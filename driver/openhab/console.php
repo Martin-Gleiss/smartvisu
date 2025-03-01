f7c0850c6c14b4a189821bdef44281f61b70dd5e
PatrikG <40170469+PatrikG8@users.noreply.github.com>
2025-03-01 20:10:47 +0100

    Driver openHAB 2.6.0

--- driver/openhab/console.php
+++ driver/openhab/console.php
@@ -5,30 +5,51 @@
  * @author      Patrik Germann
  * @copyright   2022
  * @license     GPL [http://www.gnu.de]
- * @version     1.0.1
+ * @version     2.0.0
  * -----------------------------------------------------------------------------
  */
 
-// get config-variables 
-require_once '../../lib/includes.php';
-
-set_include_path(get_include_path() . PATH_SEPARATOR . 'phpseclib');
-include('Net/SSH2.php');
-
-$ssh = new Net_SSH2(config_driver_address, config_driver_consoleport);
-if (!$ssh->login(config_driver_consoleusername, config_driver_consolepassword)) {
-	header("HTTP/1.0 603 smartVISU Driver Error");
-	$return = array('title' => 'Driver: openHAB', 'text' => 'Console-Login failed');
-}
-
-$return = null;
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
 
 if ($_POST['cmd']) {
-	$cmd = $_POST['cmd'];
-	$response = $ssh->exec($cmd);
-	$maincmd = substr($cmd, 0, strpos($cmd, ':'));
-	if ($maincmd == 'log') {
-		preg_match_all('/(\[[0-9]+m)*([A-Z]+)\|([0-9- :.]+)\|([0-9A-Za-z.]+)\|\[[0-9;]+m(.+)\[m/', $response, $parsedlog, PREG_SET_ORDER);
+	if (substr($_POST['cmd'], 0, 11) == 'log:display') {
+		$cmd = explode(' ', $_POST['cmd']);
+		$logger = $cmd[1];
+		$count = isset($cmd[2]) ? $cmd[2] : null;
+		$_POST['cmd'] = "log:display -p '%p|%d{YYYY-MM-dd HH:mm:ss.SSS}|[%c]|%h{%m}%n' --no-color" . ($cmd[1] == 'default' ? ($count ? " -n $count" : '') : " $cmd[1]");
+	}
+
+	// get config-variables 
+	require_once '../../lib/includes.php';
+
+	if (extension_loaded('ssh2')) {
+		$ssh = ssh2_connect(config_driver_address, config_driver_consoleport);
+		if (!ssh2_auth_password($ssh, config_driver_consoleusername, config_driver_consolepassword)) {
+			header("HTTP/1.0 603 smartVISU Driver Error");
+			echo '{"title": "Driver: openHAB-Console", "text": "Login failed"}';
+			exit;
+		}
+		$ssh_stream = ssh2_exec($ssh, $_POST['cmd']);
+		stream_set_blocking($ssh_stream, true);
+		$response = $output = stream_get_contents($ssh_stream);
+	} else {
+		header("HTTP/1.0 603 smartVISU Driver Error");
+		echo '{"title": "Driver: openHAB-Console", "text": "PHP-Extension SSH2 not loaded"}';
+		exit;
+	}
+
+	if (error_get_last()) {
+		$err = error_get_last();
+		header("HTTP/1.0 603 smartVISU Driver Error");
+		echo '{"title": "Driver: openHAB-Console", "text": "' . $err['message'] . '"}';
+		exit;
+	}
+
+	$return = array();
+	
+	if (substr($_POST['cmd'], 0, 11) == 'log:display') {
+		preg_match_all('/(\[[0-9]+m)*([A-Z]+)\|([0-9- :.]+)\|\[(.+)\]\|\[[0-9;]+m(.+)\[m/', $response, $parsedlog, PREG_SET_ORDER);
 		$logarray = array();
 		foreach ($parsedlog as $line) {
 			$logarray[] = array(
@@ -39,12 +60,14 @@ if ($_POST['cmd']) {
 			);
 		}
 		$logarray = array_reverse($logarray);
-		$return = json_encode($logarray);
+		$return = array_slice($logarray, 0, $count);
 	} else {
-		$return = $response;
+		$return = explode("\r\n", $response);
 	}
+	echo json_encode($return);
+} else {
+	header("HTTP/1.0 603 smartVISU Driver Error");
+	echo '{"title": "Driver: openHAB-Console", "text": "No command"}';
 }
-
-echo $return;
 
 ?>
