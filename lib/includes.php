61661d99067aa6a2c0b6ac936c71ba227e2ec723
wvhn <17801971+wvhn@users.noreply.github.com>
2026-02-10 16:58:08 +0100

    introduce widget lib.sysinfo()

--- lib/includes.php
+++ lib/includes.php
@@ -49,16 +49,16 @@ $config = new config();
 $GLOBALS['config'] = $config->get();
 
 // we allow some config keys to be provided in the request for quick testing
-$allowed_request_keys = ['cache', 'debug', 'design', 'driver', 'driver_address', 'plot_library'];
+$allowed_request_keys = ['cache', 'debug', 'design', 'driver', 'driver_address', 'plot_library', 'svstatus'];
 
 $request = array_merge($_GET, $_POST);
 
 // define constant for all config values (for backward compatibility) and allow selected request keys to override the configuration
 foreach ($GLOBALS['config'] as $key => $value) {
-	if (in_Array($key, $allowed_request_keys) && isset($request[$key])) 
-		define ('config_' . $key, $request[$key]);
-	else 
-		define('config_' . $key, $value);
+    if (in_Array($key, $allowed_request_keys) && isset($request[$key])) 
+        define ('config_' . $key, $request[$key]);
+    else 
+        define('config_' . $key, $value);
 }
 define ('config_version_full', config_version_major.".".config_version_minor.".".config_version_revision);
 
@@ -66,18 +66,18 @@ define ('config_version_full', config_version_major.".".config_version_minor."."
  * Set proxy according to config
  */
 if($GLOBALS['config']['proxy'] == true) {
-	$proxy_opts = array(
-		'http'=>array(
-			'method' => 'GET',
-			'proxy' => config_proxy_url,
-			'request_fulluri' => True
-		)
-	);
+    $proxy_opts = array(
+        'http'=>array(
+            'method' => 'GET',
+            'proxy' => config_proxy_url,
+            'request_fulluri' => True
+        )
+    );
 
-	if(\strlen(proxy_user) > 0) {
-		$proxy_opts['http']['header'] = \sprintf('Authorization: Basic %s:%s', base64_encode(config_proxy_user), base64_encode(config_proxy_password));
-	}
-	$default = stream_context_get_default($proxy_opts);
+    if(\strlen(proxy_user) > 0) {
+        $proxy_opts['http']['header'] = \sprintf('Authorization: Basic %s:%s', base64_encode(config_proxy_user), base64_encode(config_proxy_password));
+    }
+    $default = stream_context_get_default($proxy_opts);
 }
 
 /**
@@ -90,13 +90,13 @@ date_default_timezone_set(config_timezone);
 * TO DO: make a concept for integrating php error messages into SV notifications
 */
 set_error_handler(
-	function($errno, $errstr, $errfile, $errline)
-	{
-		if (\defined('config_debug') && config_debug == 1)				
-			return false;	// hand over to standard error reporting
-		else
-			return true;	// ignore warnings
-	}
-	,E_WARNING);
+    function($errno, $errstr, $errfile, $errline)
+    {
+        if (\defined('config_debug') && config_debug == 1)                
+            return false;    // hand over to standard error reporting
+        else
+            return true;    // ignore warnings
+    }
+    ,E_WARNING);
 
 ?>
